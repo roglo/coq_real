affd0ce0f8dad95c4a20ac0554bdac8dbabd3666
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-09-28 03:03:59 +0200

    -

--- Real.v
+++ Real.v
@@ -34,15 +34,17 @@ Definition rm_add_i a b i :=
   match fst_same a b (S i) with
   | Some dj =>
       (* a[S i+di]=b[S i+di] *)
-      if negb (xorb a.[i] b.[i]) then
-        (* a[i]=b[i] *)
-        xorb a.[i] a.[S i + dj]
-      else
+      if xorb a.[i] b.[i] then
         (* a[i]≠b[i] *)
         negb a.[S i + dj]
+      else
+        (* a[i]=b[i] *)
+        xorb a.[i] a.[S i + dj]
   | None =>
       xorb a.[i] b.[i]
   end.
+
+Arguments rm_add_i a%rm b%rm i%nat.
 
 Definition rm_add a b := {| rm := rm_add_i a b |}.
 
@@ -78,13 +80,12 @@ symmetry in Hsba.
 apply fst_same_iff in Hsba.
 destruct sba as [di| ]; [ idtac | apply xorb_comm ].
 rewrite xorb_comm.
-remember (negb (xorb b .[ i] a .[ i])) as nx eqn:Hnx .
-symmetry in Hnx.
-destruct Hsba as (_, Hsba).
-rewrite Hsba.
-destruct nx; [ idtac | reflexivity ].
-apply negb_true_iff, xorb_eq in Hnx.
-rewrite Hnx; reflexivity.
+remember (xorb b .[ i] a .[ i]) as xab eqn:Hxab .
+symmetry in Hxab.
+destruct Hsba as (_, Hsba); rewrite Hsba.
+destruct xab; [ reflexivity | idtac ].
+apply xorb_eq in Hxab.
+rewrite Hxab; reflexivity.
 Qed.
 
 Theorem eq_fst_same : ∀ a b i,
@@ -96,79 +97,6 @@ rewrite Nat.add_0_r; split; auto.
 intros dj Hdj.
 exfalso; revert Hdj; apply Nat.nlt_0_r.
 Qed.
-
-(*
-Theorem fst_same_assoc : ∀ a b c i,
-  fst_same a (b + c)%rm i = fst_same (a + b)%rm c i.
-Proof.
-intros a b c i.
-apply fst_same_iff.
-remember (fst_same (a + b) c i) as sab eqn:Hsab .
-symmetry in Hsab.
-apply fst_same_iff in Hsab.
-destruct sab as [di| ].
- destruct Hsab as (Hne, Heq).
- split.
-  intros dj Hdji.
-bbb.
-  destruct (bool_dec a .[ i + di] b .[ i + di]) as [H₁| H₁].
-   remember a .[ i + di] as aidi eqn:H₂ .
-   symmetry in H₁, H₂.
-   destruct aidi.
-    remember (i + di - 1) as i₁.
-    assert (c .[ i₁] = xorb a .[ i₁] b .[ i₁]).
-     destruct di; [ exfalso; revert Hdji; apply Nat.nlt_0_r | idtac ].
-     rewrite Nat.add_succ_r in Heqi₁.
-     rewrite Nat.sub_succ, Nat.sub_0_r in Heqi₁.
-     subst i₁.
-bbb.
-
-intros a b c i.
-apply fst_same_iff.
-remember (fst_same (a + b) c i) as sab eqn:Hsab .
-symmetry in Hsab.
-apply fst_same_iff in Hsab.
-destruct sab as [di| ].
- destruct Hsab as (Hne, Heq).
- split.
-  intros dj Hdji.
-  destruct (bool_dec a .[ i + di] b .[ i + di]) as [H₁| H₁].
-   Focus 1.
-   unfold rm_add, rm_add_i in Heq; simpl in Heq.
-bbb.
-
-intros a b c i.
-apply fst_same_iff.
-remember (fst_same (a + b) c i) as sab eqn:Hsab .
-symmetry in Hsab.
-apply fst_same_iff in Hsab.
-destruct sab as [di| ].
- destruct Hsab as (Hne, Heq).
- split.
-  intros dj Hdji.
-  destruct (bool_dec a .[ i + di] b .[ i + di]) as [H₁| H₁].
-   Focus 1.
-bbb.
-
-  remember Hdji as H; clear HeqH.
-  apply Hne in H.
-  unfold rm_add in Heq; simpl in Heq.
-  unfold rm_add_i in Heq; simpl in Heq.
-  remember (fst_same a b (i + di)) as aidi eqn:Haidi .
-  symmetry in Haidi.
-  apply fst_same_iff in Haidi.
-  destruct aidi as [aidi| ].
-   destruct Haidi as (Haidine, Haidieq).
-   destruct (zerop aidi) as [H₁| H₁].
-    subst aidi.
-    clear Haidine.
-    rewrite Nat.add_0_r in Haidieq.
-    remember b .[ i + di] as bidi eqn:Hbidi .
-    symmetry in Hbidi.
-    destruct bidi.
-     Focus 1.
-bbb.
-*)
 
 Theorem rm_add_assoc : ∀ a b c, (a + (b + c) = (a + b) + c)%rm.
 Proof.
@@ -185,37 +113,13 @@ destruct sa as [dia| ].
  destruct Hsa as (Hsan, Hsa).
  destruct sc as [dic| ].
   destruct Hsc as (Hscn, Hsc).
-bbb.
-
-intros a b c.
-unfold rm_eq; intros i; simpl.
-unfold rm_add_i.
-remember (fst_same a (b + c) i) as sa eqn:Hsa .
-symmetry in Hsa.
-remember (fst_same (a + b) c i) as sc eqn:Hsc .
-symmetry in Hsc.
-apply fst_same_iff in Hsa.
-apply fst_same_iff in Hsc.
-destruct sa as [dia| ].
- destruct (zerop dia) as [H₁| H₁].
-  subst dia.
-  destruct Hsa as (_, Hsa).
-  rewrite Nat.add_0_r in Hsa.
-  destruct sc as [dic| ].
-   destruct (zerop dic) as [H₁| H₁].
-    subst dic.
-    destruct Hsc as (_, Hsc).
-    rewrite Nat.add_0_r in Hsc.
-    remember (fst_same a (b + c) (S i)) as sas eqn:Hsas .
-    symmetry in Hsas.
-    remember (fst_same (a + b) c (S i)) as scs eqn:Hscs .
-    symmetry in Hscs.
-    apply fst_same_iff in Hsas.
-    apply fst_same_iff in Hscs.
-    destruct sas as [dias| ].
-     destruct scs as [dics| ].
-      destruct Hsas as (Hsan, Hsas).
-      destruct Hscs as (Hscn, Hscs).
+  remember (xorb a .[ i] (b + c) .[ i]) as xa eqn:Hxa .
+  remember (xorb (a + b) .[ i] c .[ i]) as xc eqn:Hxc .
+  symmetry in Hxa, Hxc.
+  destruct xa.
+   destruct xc.
+    f_equal.
+    rewrite Hsc.
 bbb.
 
 Close Scope nat_scope.
