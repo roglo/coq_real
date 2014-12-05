37192299e4f09609ca2a62c8d49c6c6b1fe968ad
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-12-05 04:55:52 +0100

    -

--- real.ml
+++ real.ml
@@ -7,7 +7,7 @@ value rm_zero = {rm i = 0};
 value fst_carry_sure base x y i =
   loop 0 where rec loop di =
     if x.rm (i + di) + y.rm (i + di) <> base - 1 then Some di
-    else if di > 10000 then None
+    else if di > 30 then None
     else loop (di + 1)
 ;
 
@@ -31,9 +31,11 @@ value rm_sub base a b = rm_add base a (rm_opp base b);
 
 value rm_add_carry base x y = gen_carry base x y 0;
 
+value mm = 26;
+
 value f2am base x =
   let x = mod_float x 1.0 in
-  loop 100 x [] where rec loop i x list =
+  loop mm x [] where rec loop i x list =
     if i = 0 then Array.of_list (List.rev list)
     else
       let x = float base *. x in
@@ -51,7 +53,7 @@ value f2rm base x =
   { rm i = if i < Array.length a then a.(i) else 0 }
 ;
 
-value rm2f base x = am2f base (Array.init 100 x.rm);
+value rm2f base x = am2f base (Array.init mm x.rm);
 
 let b = 2 in rm2f b (rm_add b (f2rm b 0.28) (f2rm b 0.17));
 let b = 7 in rm2f b (rm_add b (f2rm b 0.28) (f2rm b 0.17));
@@ -176,13 +178,18 @@ value rm_le base x y = rm_compare base x y <> Gt;
 value rm_gt base x y = rm_compare base x y = Gt;
 value rm_ge base x y = rm_compare base x y <> Lt;
 
-value rm_div_i x y =
-  if rm_le y x then
+value rm_shift_r n pad x = { rm i = if i < n then pad else x.rm (i-n) };
+value rm_shift_l n x = { rm i = x.rm (i+n) };
+
+value rec rm_div_i x y i =
+  if i = 0 then
+    if rm_lt 2 x y then 0 else 1
+  else
+    let x = if rm_lt 2 x y then x else rm_sub 2 x y in
+    rm_div_i (rm_shift_l 1 x) y (i - 1)
 ;
 
-value rm_div x y = {rm = rm_div_i x y};
-
-value rm_shift_r n pad x = { rm i = if i < n then pad else x.rm (i-n) };
+value rm_div x y = {rm i = rm_div_i x y (i + 1)};
 
 type real = { re_abs : real_mod_1; re_power : int; re_sign : bool };
 
@@ -214,7 +221,7 @@ value f2a base x =
       if x ≥ 1.0 then loop (x /. float base) (p + 1)
       else (x, p)
   in
-  loop 100 x [] where rec loop i x list =
+  loop mm x [] where rec loop i x list =
     if i = 0 then (Array.of_list (List.rev list), p)
     else
       let x = float base *. x in
@@ -230,7 +237,7 @@ value f2r base x =
 
 value r2f base a =
   loop 0 0.0 (1. /. float base) where rec loop i x pow =
-    if i = 100 then
+    if i = mm then
       (if a.re_sign then 1. else -1.) *. x *. float base ** float a.re_power
     else
       loop (i + 1) (x +. float (a.re_abs.rm i) *. pow)
@@ -242,3 +249,13 @@ let b = 3 in r2f b (re_add b (f2r b 1.28) (f2r b 0.17));
 let b = 3 in r2f b (re_add b (f2r b 17.9) (f2r b 16.9));
 let b = 3 in r2f b (re_add b (f2r b (-16.9)) (f2r b (-17.9)));
 let b = 3 in r2f b (re_add b (f2r b (-1.28)) (f2r b 0.17));
+
+value rm2fshort base x = am2f base (Array.init 15 x.rm);
+
+Printf.printf "%.16f\n%!" (
+0.17/.0.28
+);
+
+Printf.printf "%.16f\n%!" (
+let b = 2 in (rm2fshort b (rm_div (f2rm b 0.17) (f2rm b 0.28)))
+);
