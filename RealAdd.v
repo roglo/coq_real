4332d20a4ce7b06721bf955c13eeadb14a44cd92
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-12-16 01:29:22 +0100

    -

--- RealAdd.v
+++ RealAdd.v
@@ -1494,11 +1494,43 @@ bbb.
        destruct s2 as [dj2| ]; [ idtac | clear Hc2 ].
         destruct Hs2 as (Hn2, Ht2).
         rewrite Ht2 in Hc2; discriminate Hc2.
+
+        assert (∀ dj, rm_add_i x z (0 + dj) = true) as H.
+         intros dj; simpl; apply Hs2.
+
+         apply rm_add_inf_if in H.
+         destruct H as (j, (Hj, (Hxj, Hzj))).
+         destruct (lt_eq_lt_dec j dx) as [[H1| H1]| H1].
+          pose proof (Hzj (dx - S j)%nat) as H.
+          rewrite Nat.add_succ_r, <- Nat.add_succ_l in H.
+          rewrite Nat.add_sub_assoc in H; [ idtac | assumption ].
+          rewrite Nat.add_comm, Nat.add_sub in H.
+          rewrite Ht1 in H; discriminate H.
+
+          subst j.
+          clear Hxj.
+          unfold carry in Hc3; simpl in Hc3.
+          remember (fst_same y z 0) as s3 eqn:Hs3 .
+          destruct s3 as [dj3| ]; [ idtac | discriminate Hc3 ].
+          remember Hs3 as H; clear HeqH.
+          apply fst_same_sym_iff in H; simpl in H.
+          destruct H as (Hn3, Ht3).
+          rewrite Hc3 in Ht3; symmetry in Ht3.
+          destruct (lt_dec dj3 (S dx)) as [H1| H1].
+           rewrite Hny in Hc3; [ idtac | assumption ].
+           discriminate Hc3.
+
+           apply Nat.nlt_ge in H1.
+           pose proof (Hzj (dj3 - S dx)%nat) as H.
+           rewrite Nat.add_succ_r, <- Nat.add_succ_l in H.
+           rewrite Nat.add_sub_assoc in H; [ idtac | assumption ].
+           rewrite Nat.add_comm, Nat.add_sub in H.
+           rewrite Ht3 in H; discriminate H.
 bbb.
-     0   -   dx
-  x  1   1   0   1   1   1 …
-  y  1   1   1   0   0   0 …
-  z  0   0   0   .   .
+     0   -   dx  -   j
+  x  1   1   0   1   1   1   1 …
+  y  1   1   1   0   0   0   0 …
+  z  0   0   0   .   .   1   1 …
 
 bbb.
    unfold carry in Hc1; simpl in Hc1.
