1175137f3d4f97c2aa88c4854064f73288791014
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2014-12-30 19:54:14 +0100

    -

--- RealDiv.v
+++ RealDiv.v
@@ -699,8 +699,10 @@ destruct m2; simpl.
        unfold R_abs in H; rewrite Hxn in H; simpl in H.
        rewrite Nat.sub_0_r in H; assumption.
 
-      remember 2%nat as n eqn:Hn in |-*.
-      remember (R_int (R_abs x) / 2) as u eqn:Hu.
+      do 2 rewrite <- Nat.add_1_r in Hm.
+      rewrite <- Nat.add_assoc in Hm; simpl in Hm.
+      remember 2%nat as n eqn:Hn.
+      remember (R_int (R_abs x) / 2) as u eqn:Hu .
 (*1*)
       destruct m; simpl in Hxym.
        injection Hxym; clear Hxym; intros; subst xm ym.
@@ -708,7 +710,7 @@ destruct m2; simpl.
        apply Z.eqb_neq in Hc.
        rewrite <- Z.add_assoc, Z.add_comm in Hm.
        rewrite Z2Nat.inj_add in Hm.
-        simpl in Hm.
+        simpl in Hm; move Hn at top; subst n.
         do 2 apply eq_add_S in Hm.
         rewrite <- Z2Nat.inj_0 in Hm.
         repeat (rewrite <- Z2Nat.inj_succ in Hm; [ idtac | omega ]).
@@ -720,11 +722,8 @@ destruct m2; simpl.
 
         apply R_int_abs.
 
-       rewrite andb_true_r in Hxym.
-       clear Hc.
-       rewrite <- Hu in Hxym.
-       remember (u / 2 =? 0) as c eqn:Hc .
-       symmetry in Hc.
+       rewrite andb_true_r, <- Hu in Hxym; clear Hc.
+       remember (u / 2 =? 0) as c eqn:Hc; symmetry in Hc.
        destruct c.
         injection Hxym; clear Hxym; intros; subst xm ym.
         remember (R_is_neg x) as xn eqn:Hxn .
@@ -738,6 +737,7 @@ destruct m2; simpl.
          unfold R_abs in H; rewrite Hxn in H; simpl in H.
          rewrite Nat.sub_0_r in H; assumption.
 
+        rewrite Nat.add_succ_l, <- Nat.add_succ_r in Hm.
         remember (S n) as p; subst n; rename p into n; rename Heqp into Hn.
         remember (u / 2) as p; subst u; rename p into u; rename Heqp into Hu.
 (*2*)
@@ -747,7 +747,7 @@ destruct m2; simpl.
        apply Z.eqb_neq in Hc.
        rewrite <- Z.add_assoc, Z.add_comm in Hm.
        rewrite Z2Nat.inj_add in Hm.
-        simpl in Hm.
+        simpl in Hm; move Hn at top; subst n.
         do 2 apply eq_add_S in Hm.
         rewrite <- Z2Nat.inj_0 in Hm.
         repeat (rewrite <- Z2Nat.inj_succ in Hm; [ idtac | omega ]).
@@ -759,11 +759,8 @@ destruct m2; simpl.
 
         apply R_int_abs.
 
-       rewrite andb_true_r in Hxym.
-       clear Hc.
-       rewrite <- Hu in Hxym.
-       remember (u / 2 =? 0) as c eqn:Hc .
-       symmetry in Hc.
+       rewrite andb_true_r, <- Hu in Hxym; clear Hc.
+       remember (u / 2 =? 0) as c eqn:Hc; symmetry in Hc.
        destruct c.
         injection Hxym; clear Hxym; intros; subst xm ym.
         remember (R_is_neg x) as xn eqn:Hxn .
@@ -777,6 +774,7 @@ destruct m2; simpl.
          unfold R_abs in H; rewrite Hxn in H; simpl in H.
          rewrite Nat.sub_0_r in H; assumption.
 
+        rewrite Nat.add_succ_l, <- Nat.add_succ_r in Hm.
         remember (S n) as p; subst n; rename p into n; rename Heqp into Hn.
         remember (u / 2) as p; subst u; rename p into u; rename Heqp into Hu.
 (*3*)
@@ -786,7 +784,7 @@ destruct m2; simpl.
        apply Z.eqb_neq in Hc.
        rewrite <- Z.add_assoc, Z.add_comm in Hm.
        rewrite Z2Nat.inj_add in Hm.
-        simpl in Hm.
+        simpl in Hm; move Hn at top; subst n.
         do 2 apply eq_add_S in Hm.
         rewrite <- Z2Nat.inj_0 in Hm.
         repeat (rewrite <- Z2Nat.inj_succ in Hm; [ idtac | omega ]).
@@ -798,11 +796,8 @@ destruct m2; simpl.
 
         apply R_int_abs.
 
-       rewrite andb_true_r in Hxym.
-       clear Hc.
-       rewrite <- Hu in Hxym.
-       remember (u / 2 =? 0) as c eqn:Hc .
-       symmetry in Hc.
+       rewrite andb_true_r, <- Hu in Hxym; clear Hc.
+       remember (u / 2 =? 0) as c eqn:Hc; symmetry in Hc.
        destruct c.
         injection Hxym; clear Hxym; intros; subst xm ym.
         remember (R_is_neg x) as xn eqn:Hxn .
@@ -816,6 +811,7 @@ destruct m2; simpl.
          unfold R_abs in H; rewrite Hxn in H; simpl in H.
          rewrite Nat.sub_0_r in H; assumption.
 
+        rewrite Nat.add_succ_l, <- Nat.add_succ_r in Hm.
         remember (S n) as p; subst n; rename p into n; rename Heqp into Hn.
         remember (u / 2) as p; subst u; rename p into u; rename Heqp into Hu.
 bbb.
