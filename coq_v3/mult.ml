8b5a740713aa2c5744ddc42aa6f3c12108c3c1fb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-02-05 12:03:07 +0100

    -

--- coq_v3/mult.ml
+++ coq_v3/mult.ml
@@ -10,6 +10,15 @@ value rec summation_loop b len g =
 
 value summation b e g = summation_loop b (e + 1 - b) g;
 
+value base = ref 2;
+value propag_carry u i = u i mod base.val + u (i + 1) / base.val;
+value rec propag_carry_sev_times u n =
+  if n ≤ 0 then u
+  else propag_carry_sev_times (propag_carry u) (n-1)
+;
+
 value i_mul_i x y i =
-  summation 1 i (fun j → b2n (x.rm (j - 1)) * b2n (y.rm (i - j)))
+  let u k = summation 1 k (fun j → b2n (x.rm (j - 1)) * b2n (y.rm (k - j))) in
+  let n = n such that ((base^n-1)/(base-1)-n) ≥ i
+  propag_carry_sev_times u n i
 ;
