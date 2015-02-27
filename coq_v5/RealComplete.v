5e86fde3e7e4a8573b72c800ad95ed4d142b65fe
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-02-27 09:54:25 +0100

    -

--- coq_v5/RealComplete.v
+++ coq_v5/RealComplete.v
@@ -1,22 +1,26 @@
-(* ℝ is complete *)
+(* I is complete *)
 
 Require Import Utf8 QArith NPeano Misc.
-Require Import Real01 Real RealAdd RealAddGrp.
+Require Import Real01 Real01Cmp Real01Add.
 
 Open Scope nat_scope.
 
+Definition I_abs_sub x y := if I_lt_dec x y then (y - x)%I else (x - y)%I.
+
 Definition cauchy_sequence u :=
-  ∀ ε, (ε > 0)%R → ∃ N, ∀ p q, p > N → q > N → (R_abs (u p - u q) < ε)%R.
+  ∀ ε, (ε ≠ 0)%I → ∃ N, ∀ p q, p > N → q > N → (I_abs_sub (u p) (u q) < ε)%I.
 
 Definition converges_to u r :=
-  ∀ ε, (ε > 0)%R → ∃ N, ∀ n, n > N → (R_abs (r - u n) < ε)%R.
+  ∀ ε, (ε ≠ 0)%I → ∃ N, ∀ n, n > N → (I_abs_sub r (u n) < ε)%I.
 
 Theorem zzz : ∀ u, cauchy_sequence u → ∃ r, converges_to u r.
 Proof.
 intros u Hc.
 unfold cauchy_sequence in Hc.
 unfold converges_to.
-assert (1 > 0)%R as H.
+bbb.
+
+assert (1 > 0)%I as H.
  Focus 2.
  apply Hc in H.
  destruct H as (N, HN).
