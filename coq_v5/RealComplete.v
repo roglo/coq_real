d0a11216c77655f8b48b9096890b2f4a53e1adc9
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-03-03 00:53:09 +0100

    -

--- coq_v5/RealComplete.v
+++ coq_v5/RealComplete.v
@@ -46,10 +46,71 @@ destruct (R_lt_dec x y) as [H1| H1]; [ idtac | apply R_le_refl ].
 apply R_lt_le_incl; assumption.
 Qed.
 
+Theorem R_lt_add_compat : ∀ x y z, (x < y)%R → (x + z < y + z)%R.
+Proof.
+intros x y z Hxy.
+unfold R_lt in Hxy; unfold R_lt.
+unfold R_compare in Hxy; unfold R_compare.
+remember (R_norm x) as nx eqn:Hnx.
+remember (R_norm y) as ny eqn:Hny.
+remember (R_norm z) as nz eqn:Hnz.
+remember (R_norm (x + z)) as nxz eqn:Hnxz.
+remember (R_norm (y + z)) as nyz eqn:Hnyz.
+remember (R_int nx ?= R_int ny)%Z as c eqn:Hc.
+symmetry in Hc.
+destruct c.
+ apply Z.compare_eq in Hc.
+ remember (fst_same (R_frac nx) (- R_frac ny) 0) as s1 eqn:Hs1.
+ apply fst_same_sym_iff in Hs1; simpl in Hs1.
+ destruct s1 as [di1| ].
+  destruct Hs1 as (Hn1, Ht1).
+  destruct (Digit.dec (R_frac nx .[ di1])) as [H1| H1].
+   discriminate Hxy.
+
+   clear Hxy.
+   rewrite H1 in Ht1; apply Digit.opp_sym in Ht1.
+   remember ((R_int nxz ?= R_int nyz)%Z) as c1 eqn:Hc1.
+   symmetry in Hc1.
+   destruct c1.
+    apply Z.compare_eq in Hc1.
+    remember (fst_same (R_frac nxz) (- R_frac nyz) 0) as s2 eqn:Hs2.
+    apply fst_same_sym_iff in Hs2; simpl in Hs2.
+    destruct s2 as [di2| ].
+     destruct Hs2 as (Hn2, Ht2).
+     destruct (Digit.dec (R_frac nxz .[ di2])) as [H2| H2].
+      rewrite H2 in Ht2; apply Digit.opp_sym in Ht2.
+      exfalso.
+      move ny before nx; move nz before ny.
+      move nxz before ny; move nyz before nxz.
+      move Hnz before Hny; move Hnxz before Hnz.
+      move Hnyz before Hnxz; move Hc1 before Hc.
+      destruct (lt_eq_lt_dec di1 di2) as [[H3| H3]| H3].
+       remember H3 as H; clear HeqH.
+       apply Hn2 in H; simpl in H.
+       rewrite Digit.opp_involutive in H.
+       (* part en couille peut-être ici *)
+       rewrite Hnx, Hny in Hc.
+       unfold R_norm in Hc; simpl in Hc.
+       rewrite Hnxz, Hnyz in Hc1.
+       unfold R_norm in Hc1; simpl in Hc1.
+       rewrite Hny in Ht1; simpl in Ht1.
+       rewrite Hnx in H1; simpl in H1.
+       rewrite Hnyz in Ht2; simpl in Ht2.
+       rewrite Hnxz in H2; simpl in H2.
+       rewrite Hnxz, Hnyz in H; simpl in H.
+       unfold I_add_i in H; simpl in H.
+       do 2 rewrite Digit.add_0_r in H.
+       unfold I_add_i in H; simpl in H.
+       remember (R_frac x) as xf eqn:Hxf.
+       remember (R_frac y) as yf eqn:Hyf.
+       remember (R_frac z) as zf eqn:Hzf.
+bbb. bof.
+
 Theorem R_lt_sub_lt_add_l : ∀ x y z, (x - y < z)%R ↔ (x < y + z)%R.
 Proof.
 intros x y z.
 split; intros Hxyz.
+SearchAbout I_le.
 bbb.
 
 Theorem R_le_pos_lt_compat : ∀ x y z, (x ≤ y)%R → (z > 0)%R → (x < y + z)%R.
