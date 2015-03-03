8328fae649fe78152776f28f1fa3a7e6eae43984
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-03-03 06:01:28 +0100

    -

--- coq_v5/RealComplete.v
+++ coq_v5/RealComplete.v
@@ -46,21 +46,36 @@ destruct (R_lt_dec x y) as [H1| H1]; [ idtac | apply R_le_refl ].
 apply R_lt_le_incl; assumption.
 Qed.
 
-Theorem R_lt_add_compat : ∀ x y z, (x < y)%R → (x + z < y + z)%R.
+Theorem R_compare_add_compat : ∀ x y z, (x ?= y)%R = (x + z ?= y + z)%R.
 Proof.
-intros x y z Hxy.
-unfold R_lt in Hxy; unfold R_lt.
-unfold R_compare in Hxy; unfold R_compare.
+intros x y z.
+unfold R_compare.
 remember (R_norm x) as nx eqn:Hnx.
 remember (R_norm y) as ny eqn:Hny.
 remember (R_norm z) as nz eqn:Hnz.
 remember (R_norm (x + z)) as nxz eqn:Hnxz.
 remember (R_norm (y + z)) as nyz eqn:Hnyz.
-remember (R_int nx ?= R_int ny)%Z as c eqn:Hc.
-symmetry in Hc.
+remember (R_int nx ?= R_int ny)%Z as c1 eqn:Hc1.
+remember (R_int nxz ?= R_int nyz)%Z as c2 eqn:Hc2.
+symmetry in Hc1, Hc2.
+remember (fst_same (R_frac nx) (- R_frac ny) 0) as s1 eqn:Hs1.
+remember (fst_same (R_frac nxz) (- R_frac nyz) 0) as s2 eqn:Hs2.
+destruct c1, c2; try reflexivity.
+ apply Z.compare_eq in Hc1.
+ apply Z.compare_eq in Hc2.
+ destruct s1 as [j1| ].
+  apply fst_same_sym_iff in Hs1; simpl in Hs1.
+  destruct Hs1 as (Hn1, Ht1).
+  destruct s2 as [j2| ].
+   apply fst_same_sym_iff in Hs2; simpl in Hs2.
+   destruct Hs2 as (Hn2, Ht2).
+   destruct (Digit.dec (R_frac nx .[ j1])) as [H1| H1].
+    destruct (Digit.dec (R_frac nxz .[ j2])) as [H2| H2]; auto; exfalso.
+    subst; simpl in *.
+bbb.
+
 destruct c.
  apply Z.compare_eq in Hc.
- remember (fst_same (R_frac nx) (- R_frac ny) 0) as s1 eqn:Hs1.
  apply fst_same_sym_iff in Hs1; simpl in Hs1.
  destruct s1 as [di1| ].
   destruct Hs1 as (Hn1, Ht1).
@@ -69,11 +84,9 @@ destruct c.
 
    clear Hxy.
    rewrite H1 in Ht1; apply Digit.opp_sym in Ht1.
-   remember ((R_int nxz ?= R_int nyz)%Z) as c1 eqn:Hc1.
-   symmetry in Hc1.
-   destruct c1.
-    apply Z.compare_eq in Hc1.
-    remember (fst_same (R_frac nxz) (- R_frac nyz) 0) as s2 eqn:Hs2.
+   symmetry in Hc2.
+   destruct c2.
+    apply Z.compare_eq in Hc2.
     apply fst_same_sym_iff in Hs2; simpl in Hs2.
     destruct s2 as [di2| ].
      destruct Hs2 as (Hn2, Ht2).
@@ -83,7 +96,7 @@ destruct c.
       move ny before nx; move nz before ny.
       move nxz before ny; move nyz before nxz.
       move Hnz before Hny; move Hnxz before Hnz.
-      move Hnyz before Hnxz; move Hc1 before Hc.
+      move Hnyz before Hnxz; move Hc2 before Hc.
       destruct (lt_eq_lt_dec di1 di2) as [[H3| H3]| H3].
        remember H3 as H; clear HeqH.
        apply Hn2 in H; simpl in H.
@@ -91,8 +104,8 @@ destruct c.
        (* part en couille peut-être ici *)
        rewrite Hnx, Hny in Hc.
        unfold R_norm in Hc; simpl in Hc.
-       rewrite Hnxz, Hnyz in Hc1.
-       unfold R_norm in Hc1; simpl in Hc1.
+       rewrite Hnxz, Hnyz in Hc2.
+       unfold R_norm in Hc2; simpl in Hc2.
        rewrite Hny in Ht1; simpl in Ht1.
        rewrite Hnx in H1; simpl in H1.
        rewrite Hnyz in Ht2; simpl in Ht2.
@@ -104,7 +117,11 @@ destruct c.
        remember (R_frac x) as xf eqn:Hxf.
        remember (R_frac y) as yf eqn:Hyf.
        remember (R_frac z) as zf eqn:Hzf.
+Abort. (*
 bbb. bof.
+*)
+
+SearchAbout R_compare.
 
 Theorem R_lt_sub_lt_add_l : ∀ x y z, (x - y < z)%R ↔ (x < y + z)%R.
 Proof.
