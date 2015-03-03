fa6f1831b6bbcf404cf964784db336cca878eb50
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-03-03 18:42:33 +0100

    -

--- coq_v5/RealComplete.v
+++ coq_v5/RealComplete.v
@@ -49,7 +49,106 @@ Qed.
 Theorem R_lt_add_compat_r : ∀ x y z, (x < y)%R → (x + z < y + z)%R.
 Proof.
 intros x y z Hxy.
+unfold R_lt in Hxy; unfold R_lt.
+unfold R_compare in Hxy; unfold R_compare.
+remember (R_norm x) as nx eqn:Hnx.
+remember (R_norm y) as ny eqn:Hny.
+remember (R_norm (x + z)) as nxz eqn:Hnxz.
+remember (R_norm (y + z)) as nyz eqn:Hnyz.
+remember (R_int nx ?= R_int ny)%Z as c1 eqn:Hc1.
+remember (R_int nxz ?= R_int nyz)%Z as c2 eqn:Hc2.
+symmetry in Hc1, Hc2.
+remember (fst_same (R_frac nx) (- R_frac ny) 0) as s1 eqn:Hs1.
+remember (fst_same (R_frac nxz) (- R_frac nyz) 0) as s2 eqn:Hs2.
+destruct c1; [ idtac | clear Hxy | discriminate Hxy ].
+ apply Z.compare_eq in Hc1.
+ destruct s1 as [j1| ]; [| discriminate Hxy ].
+ destruct (Digit.dec (R_frac nx .[ j1])) as [H1| H1].
+  discriminate Hxy.
+
+  clear Hxy.
+  apply fst_same_sym_iff in Hs1; simpl in Hs1.
+  destruct Hs1 as (Hn1, Ht1).
+  rewrite H1 in Ht1; rename H1 into Hnx1; rename Ht1 into Hny1.
+  apply Digit.opp_sym in Hny1; rewrite Digit.opp_0 in Hny1.
+  destruct c2; [ idtac | reflexivity | exfalso ].
+   apply Z.compare_eq in Hc2.
+   destruct s2 as [j2| ]; [ idtac | exfalso ].
+    destruct (Digit.dec (R_frac nxz .[ j2])) as [H2| H2].
+     2: reflexivity.
+     exfalso.
+     apply fst_same_sym_iff in Hs2; simpl in Hs2.
+     destruct Hs2 as (Hn2, Ht2).
+     rewrite H2 in Ht2; rename H2 into Hnxz2; rename Ht2 into Hnyz2.
+     apply Digit.opp_sym in Hnyz2; rewrite Digit.opp_1 in Hnyz2.
 bbb.
+
+   Focus 2.
+   apply Z.compare_gt_iff in Hc2.
+   rewrite Hnyz, Hnxz in Hc2.
+   unfold R_norm in Hc2; simpl in Hc2.
+   rewrite Hnx, Hny in Hc1; simpl in Hc1.
+   do 4 rewrite <- Z.add_assoc in Hc2.
+   rewrite Z.add_comm in Hc2; apply Z.lt_gt in Hc2.
+   rewrite Z.add_comm in Hc2; apply Z.gt_lt in Hc2.
+   do 4 rewrite <- Z.add_assoc in Hc2.
+   apply Z.add_lt_mono_l in Hc2.
+   do 2 rewrite Z.add_assoc in Hc2.
+   rewrite Z.add_shuffle0, <- Z.add_assoc in Hc2.
+
+
+   rewrite Hc1 in Hc2.
+   rewrite Hny in 
+
+SearchAbout (_ = 1)%D.
+  remember Hnx1 as H; clear HeqH.
+Focus 1.
+  eapply I_eq_neq_prop in H; [ idtac | idtac | eassumption ].
+bbb.
+
+
+destruct c1, c2; try reflexivity.
+ apply Z.compare_eq in Hc2.
+ destruct s1 as [j1| ].
+  apply fst_same_sym_iff in Hs1; simpl in Hs1.
+  destruct Hs1 as (Hn1, Ht1).
+  destruct s2 as [j2| ].
+   apply fst_same_sym_iff in Hs2; simpl in Hs2.
+   destruct Hs2 as (Hn2, Ht2).
+   destruct (Digit.dec (R_frac nx .[ j1])) as [H1| H1].
+    destruct (Digit.dec (R_frac nxz .[ j2])) as [H2| H2]; auto; exfalso.
+    subst; simpl in *.
+    rewrite H2 in Ht2.
+    apply Digit.opp_sym in Ht2; rewrite Digit.opp_0 in Ht2.
+    rewrite H1 in Ht1.
+    apply Digit.opp_sym in Ht1; rewrite Digit.opp_1 in Ht1.
+    unfold I_add_i in H1; simpl in H1.
+    unfold I_add_i in H2; simpl in H2.
+    unfold I_add_i in Ht1; simpl in Ht1.
+    unfold I_add_i in Ht2; simpl in Ht2.
+    rewrite Digit.add_0_r in H1, H2, Ht1, Ht2.
+    unfold I_add_i in H2; simpl in H2.
+    unfold I_add_i in Ht2; simpl in Ht2.
+    remember (R_frac x) as xf eqn:Hxf.
+    remember (R_frac y) as yf eqn:Hyf.
+    remember (R_frac z) as zf eqn:Hzf.
+    remember (R_int x) as xi eqn:Hxi.
+    remember (R_int y) as yi eqn:Hyi.
+    remember (R_int z) as zi eqn:Hzi.
+    move Hyf before Hxf; move yf before xf; move zf before yf.
+    move xi before zf; move yi before xi; move zi before yi.
+    move Hxi before Hzf; move Hyi before Hxi; move Hzi before Hyi.
+    move H1 after H2; move Hc1 after Hc2.
+    do 4 rewrite <- Z.add_assoc in Hc2.
+    rewrite Z.add_comm in Hc2; symmetry in Hc2.
+    rewrite Z.add_comm in Hc2; symmetry in Hc2.
+    do 4 rewrite <- Z.add_assoc in Hc2.
+    apply Z.add_reg_l in Hc2.
+    rewrite Z.add_comm in Hc2; symmetry in Hc2.
+    rewrite Z.add_comm in Hc2; symmetry in Hc2.
+    move Ht1 before H1; move Ht2 before H2.
+bbb.
+*)
 
 Theorem R_compare_add_compat_r : ∀ x y z, (x ?= y)%R = (x + z ?= y + z)%R.
 Proof.
@@ -96,68 +195,6 @@ apply R_lt_add_compat_r.
 
 
 bbb.
-
-   apply R_add_compat with (x:=(-z)%R) (y:=(-z)%R) in Hc2.
-
-
- apply R_compare_gt in Hc2.
- rewrite Hc1 in 
-
-
-unfold R_compare.
-remember (R_norm x) as nx eqn:Hnx.
-remember (R_norm y) as ny eqn:Hny.
-remember (R_norm z) as nz eqn:Hnz.
-remember (R_norm (x + z)) as nxz eqn:Hnxz.
-remember (R_norm (y + z)) as nyz eqn:Hnyz.
-remember (R_int nx ?= R_int ny)%Z as c1 eqn:Hc1.
-remember (R_int nxz ?= R_int nyz)%Z as c2 eqn:Hc2.
-symmetry in Hc1, Hc2.
-remember (fst_same (R_frac nx) (- R_frac ny) 0) as s1 eqn:Hs1.
-remember (fst_same (R_frac nxz) (- R_frac nyz) 0) as s2 eqn:Hs2.
-destruct c1, c2; try reflexivity.
- apply Z.compare_eq in Hc1.
- apply Z.compare_eq in Hc2.
- destruct s1 as [j1| ].
-  apply fst_same_sym_iff in Hs1; simpl in Hs1.
-  destruct Hs1 as (Hn1, Ht1).
-  destruct s2 as [j2| ].
-   apply fst_same_sym_iff in Hs2; simpl in Hs2.
-   destruct Hs2 as (Hn2, Ht2).
-   destruct (Digit.dec (R_frac nx .[ j1])) as [H1| H1].
-    destruct (Digit.dec (R_frac nxz .[ j2])) as [H2| H2]; auto; exfalso.
-    subst; simpl in *.
-    rewrite H2 in Ht2.
-    apply Digit.opp_sym in Ht2; rewrite Digit.opp_0 in Ht2.
-    rewrite H1 in Ht1.
-    apply Digit.opp_sym in Ht1; rewrite Digit.opp_1 in Ht1.
-    unfold I_add_i in H1; simpl in H1.
-    unfold I_add_i in H2; simpl in H2.
-    unfold I_add_i in Ht1; simpl in Ht1.
-    unfold I_add_i in Ht2; simpl in Ht2.
-    rewrite Digit.add_0_r in H1, H2, Ht1, Ht2.
-    unfold I_add_i in H2; simpl in H2.
-    unfold I_add_i in Ht2; simpl in Ht2.
-    remember (R_frac x) as xf eqn:Hxf.
-    remember (R_frac y) as yf eqn:Hyf.
-    remember (R_frac z) as zf eqn:Hzf.
-    remember (R_int x) as xi eqn:Hxi.
-    remember (R_int y) as yi eqn:Hyi.
-    remember (R_int z) as zi eqn:Hzi.
-    move Hyf before Hxf; move yf before xf; move zf before yf.
-    move xi before zf; move yi before xi; move zi before yi.
-    move Hxi before Hzf; move Hyi before Hxi; move Hzi before Hyi.
-    move H1 after H2; move Hc1 after Hc2.
-    do 4 rewrite <- Z.add_assoc in Hc2.
-    rewrite Z.add_comm in Hc2; symmetry in Hc2.
-    rewrite Z.add_comm in Hc2; symmetry in Hc2.
-    do 4 rewrite <- Z.add_assoc in Hc2.
-    apply Z.add_reg_l in Hc2.
-    rewrite Z.add_comm in Hc2; symmetry in Hc2.
-    rewrite Z.add_comm in Hc2; symmetry in Hc2.
-    move Ht1 before H1; move Ht2 before H2.
-bbb.
-*)
 
 SearchAbout R_compare.
 
