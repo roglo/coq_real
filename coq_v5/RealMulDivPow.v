5a9528a4f0e9e09ec5d5b098a67800b59771a3e6
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-03-11 03:51:28 +0100

    -

--- coq_v5/RealMulDivPow.v
+++ coq_v5/RealMulDivPow.v
@@ -3,54 +3,54 @@
 Require Import Utf8 QArith NPeano.
 Require Import Digit Real01 Real01Add Real RealAdd.
 
-Definition I_mul_2_pow x n := {| rm i := x.[i+n] |}.
-Arguments I_mul_2_pow x%I n%nat.
+Definition I_mul_b_pow x n := {| rm i := x.[i+n] |}.
+Arguments I_mul_b_pow x%I n%nat.
 
-Definition I_div_2_pow_i x n i := if lt_dec i n then 0%D else x.[i-n].
-Arguments I_div_2_pow_i x%I n%nat i%nat.
+Definition I_div_b_pow_i x n i := if lt_dec i n then 0%D else x.[i-n].
+Arguments I_div_b_pow_i x%I n%nat i%nat.
 
-Definition I_div_2_pow x n := {| rm := I_div_2_pow_i x n |}.
-Arguments I_div_2_pow x%I n%nat.
+Definition I_div_b_pow x n := {| rm := I_div_b_pow_i x n |}.
+Arguments I_div_b_pow x%I n%nat.
 
-Fixpoint I_mul_2_pow_from xi xf n :=
+Fixpoint I_mul_b_pow_from xi xf n :=
   match n with
   | 0 => xi
-  | S n1 => I_mul_2_pow_from (2 * xi + b2z (xf.[n]))%Z xf n1
+  | S n1 => I_mul_b_pow_from (2 * xi + b2z (xf.[n]))%Z xf n1
   end.
 
-Fixpoint I_div_2_pow_from_int xi n :=
+Fixpoint I_div_b_pow_from_int xi n :=
   match n with
   | 0 => if Z_zerop (xi mod 2) then 0%D else 1%D
-  | S n1 => I_div_2_pow_from_int (xi / 2)%Z n1
+  | S n1 => I_div_b_pow_from_int (xi / 2)%Z n1
   end.
 
-Definition I_div_2_pow_frac_i xi xf n i :=
-  if lt_dec i n then I_div_2_pow_from_int xi (n-i)
+Definition I_div_b_pow_frac_i xi xf n i :=
+  if lt_dec i n then I_div_b_pow_from_int xi (pred (n-i))
   else xf.[i-n].
 
-Fixpoint I_div_2_pow_int xi n :=
+Fixpoint I_div_b_pow_int xi n :=
   match n with
   | 0 => xi
-  | S n1 => I_div_2_pow_int (xi / 2)%Z n1
+  | S n1 => I_div_b_pow_int (xi / 2)%Z n1
   end.
 
-Definition I_div_2_pow_frac xi xf n := {| rm := I_div_2_pow_frac_i xi xf n |}.
+Definition I_div_b_pow_frac xi xf n := {| rm := I_div_b_pow_frac_i xi xf n |}.
 
-Definition R_mul_2_pow x n :=
-  {| R_int := I_mul_2_pow_from (R_int x) (R_frac x) n;
-     R_frac := I_mul_2_pow (R_frac x) n |}.
+Definition R_mul_b_pow x n :=
+  {| R_int := I_mul_b_pow_from (R_int x) (R_frac x) n;
+     R_frac := I_mul_b_pow (R_frac x) n |}.
 
-Definition R_div_2_pow x n :=
-  {| R_int := I_div_2_pow_int (R_int x) n;
-     R_frac := I_div_2_pow_frac (R_int x) (R_frac x) n |}.
+Definition R_div_b_pow x n :=
+  {| R_int := I_div_b_pow_int (R_int x) n;
+     R_frac := I_div_b_pow_frac (R_int x) (R_frac x) n |}.
 
-Theorem I_mul_2_pow_div : ∀ x n, (I_mul_2_pow (I_div_2_pow x n) n = x)%I.
+Theorem I_mul_b_pow_div : ∀ x n, (I_mul_b_pow (I_div_b_pow x n) n = x)%I.
 Proof.
 intros x n.
 unfold I_eq; intros i; simpl.
 unfold I_add_i; simpl.
 do 2 rewrite Digit.add_0_r.
-unfold I_div_2_pow_i; simpl.
+unfold I_div_b_pow_i; simpl.
 rewrite Nat.add_sub.
 destruct (lt_dec (i + n) n) as [H1| H1].
  apply Nat.lt_add_lt_sub_r in H1.
@@ -60,7 +60,7 @@ destruct (lt_dec (i + n) n) as [H1| H1].
  clear H1.
  apply Digit.add_compat; [ reflexivity | idtac ].
  unfold carry; simpl.
- remember (fst_same (I_mul_2_pow (I_div_2_pow x n) n) 0 (S i)) as s1 eqn:Hs1 .
+ remember (fst_same (I_mul_b_pow (I_div_b_pow x n) n) 0 (S i)) as s1 eqn:Hs1 .
  remember (fst_same x 0 (S i)) as s2 eqn:Hs2 .
  destruct s1 as [dj1| ].
   apply fst_same_sym_iff in Hs1; simpl in Hs1.
@@ -69,7 +69,7 @@ destruct (lt_dec (i + n) n) as [H1| H1].
   destruct s2 as [dj2| ].
    destruct Hs2 as (Hn2, Ht2); rewrite Ht2; reflexivity.
 
-   unfold I_div_2_pow_i in Ht1; simpl in Ht1.
+   unfold I_div_b_pow_i in Ht1; simpl in Ht1.
    destruct (lt_dec (S (i + dj1 + n)) n) as [H1| H1].
     rewrite <- Nat.add_succ_l in H1.
     apply Nat.lt_add_lt_sub_r in H1.
@@ -89,7 +89,7 @@ destruct (lt_dec (i + n) n) as [H1| H1].
   apply fst_same_sym_iff in Hs2; simpl in Hs2.
   destruct Hs2 as (Hn2, Ht2).
   pose proof (Hs1 dj2) as H.
-  unfold I_div_2_pow_i in H; simpl in H.
+  unfold I_div_b_pow_i in H; simpl in H.
   destruct (lt_dec (S (i + dj2 + n)) n) as [H1| H1].
    discr_digit H.
 
