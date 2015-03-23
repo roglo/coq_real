3987a3db45d1672257f8fba40f599e06083903f0
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-03-23 19:18:42 +0100

    -

--- coq_v5/Real01Mul.v
+++ coq_v5/Real01Mul.v
@@ -706,6 +706,26 @@ destruct (I_eqs_dec (x + 0)%I x) as [H1| H1].
             apply Nat_lt_add_sub_lt_l; apply le_n_S; assumption.
 
             simpl.
+            erewrite summation_compat.
+             Focus 2.
+             intros k Hk.
+             rewrite all_0_summation_0.
+              rewrite Nat.mul_0_l, Nat.add_0_r; reflexivity.
+
+              intros l (Hl, Hlk).
+              pose proof Hx (l - S (S i)) as H.
+              rewrite Nat.add_succ_r, <- Nat.add_succ_l in H.
+              remember (S (S i)) as ssi.
+              replace (S i) with (pred (S (S i))) in H by apply Nat.pred_succ.
+              subst ssi.
+              rewrite Nat.add_pred_l in H; [ idtac | intros I; discriminate I ].
+              rewrite Nat.add_sub_assoc in H; [ idtac | assumption ].
+              rewrite Nat.add_comm, Nat.add_sub in H.
+              rewrite <- Nat.sub_1_r, digit_d2n_eq_iff in H.
+              rewrite digit_d2n_eq_iff in H2.
+              rewrite H, H2, d2n_0, Nat.mul_0_l; reflexivity.
+
+             simpl.
 bbb.
      .   i   .   .
   x  .   1   0   0   0 …
