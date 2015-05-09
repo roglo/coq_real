d44ff63fa258abb380dff970f3a2d7cfc3176263
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-05-09 11:47:04 +0200

    -

--- coq_v5/Real01Add2.v
+++ coq_v5/Real01Add2.v
@@ -2874,7 +2874,6 @@ destruct Hxy as [Hxy| Hxy].
       generalize Hs2; intros H.
       apply first_nonzero_iff in H.
       rename H into Hn2.
-exfalso.
 bbb.
       destruct s3 as [n3| ].
        pose proof Hxy (S (j + n3)) as (_, H).
