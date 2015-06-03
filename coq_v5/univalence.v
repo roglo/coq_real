b76e84b10af6048cbf23d8a75237138a7df89f1e
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-03 02:00:06 +0200

    -

--- coq_v5/univalence.v
+++ coq_v5/univalence.v
@@ -8,11 +8,11 @@ Definition equiv A B :=
 
 Notation "A ≃ B" := (equiv A B) (at level 60).
 
-Axiom univalence : ∀ A B, (A ≃ B) ≃ (A = B).
+Axiom univalence : ∀ A B : Set, (A ≃ B) ≃ (A = B).
 
 Inductive t := foo : t | bar : t.
 
-Theorem bool_eq_t : @eq Type bool t.
+Theorem bool_eq_t : bool = t.
 Proof.
 pose proof univalence bool t as H.
 unfold equiv at 1 in H.
@@ -23,3 +23,16 @@ exists (λ b : bool, if b then foo else bar).
 exists (λ b : t, if b then true else false).
 split; intros x; destruct x; reflexivity.
 Qed.
+
+Definition negt : t → t.
+Proof.
+rewrite <- bool_eq_t.
+apply negb.
+Defined.
+
+Theorem aaa : negt foo = bar.
+Proof.
+unfold negt.
+unfold eq_rec; simpl.
+unfold eq_rect; simpl.
+bbb.
