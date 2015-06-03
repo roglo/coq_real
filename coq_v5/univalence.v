68b6648ce8fb8f08af68ca31e74642c24b8ade36
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-04 00:51:58 +0200

    -

--- coq_v5/univalence.v
+++ coq_v5/univalence.v
@@ -2,6 +2,65 @@
 
 Require Import Utf8.
 
+(* version Jérémy *)
+
+Definition is_equiv {A B : Set} (f : A → B) :=
+ ∃ g : B → A,
+ (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y).
+
+Definition equiv (A B : Set) := ∃ f : A → B, is_equiv f.
+Notation "A ≃ B" := (equiv A B) (at level 60).
+
+Definition eq_to_equiv A B : A = B → A ≃ B.
+Proof.
+intros H.
+subst B.
+unfold equiv, is_equiv.
+exists (λ x, x), (λ x, x).
+split; intros x; reflexivity.
+Defined.
+
+Print eq_to_equiv.
+
+Axiom univalence : ∀ A B, is_equiv (eq_to_equiv A B).
+
+Inductive t := foo : t | bar : t.
+
+Theorem bool_equiv_t : bool ≃ t.
+Proof.
+exists (λ b : bool, if b then foo else bar).
+exists (λ b : t, if b then true else false).
+split; intros x; destruct x; reflexivity.
+Defined.
+
+Theorem bool_eq_t : bool = t.
+Proof.
+apply univalence.
+apply bool_equiv_t.
+Defined.
+
+Print bool_eq_t.
+
+Definition negt : t → t.
+Proof.
+rewrite <- bool_eq_t.
+apply negb.
+Defined.
+
+Theorem aaa : negt foo = bar.
+Proof.
+unfold negt.
+unfold negb; simpl.
+unfold eq_rec; simpl.
+unfold eq_rect; simpl.
+unfold bool_eq_t.
+unfold bool_equiv_t.
+remember (univalence bool t) as v eqn:Hv.
+destruct v as (v, (f, g)).
+bbb.
+
+(* ma version initiale *)
+
 Definition equiv A B :=
   ∃ f : A → B, ∃ g : B → A,
   (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y).
