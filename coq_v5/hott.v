111f7af325a86c599d37504cc9e437df9645481c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-06 11:02:22 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -11,37 +11,34 @@ Open Scope nat_scope.
 Inductive Id {A} : A → A → Type :=
   | refl : ∀ x : A, Id x x.
 
-Definition indiscernable_identical {A} (C : A → Type) x y (p : Id x y) cx :=
-  match p in (Id a b) return (C a → C b) with
-  | refl _ => id
-  end cx.
-
-Theorem indiscernability : ∀ A (C : A → Set),
-  ∃ (f : ∀ x y (p : Id x y), C x → C y),
+Theorem indiscernability : ∀ A (P : A → Set),
+  ∃ (f : ∀ x y (p : Id x y), P x → P y),
   ∀ x, f x x (refl x) = id.
 Proof.
-intros A C.
-exists (indiscernable_identical C).
+intros A P.
+exists
+  (λ x y p,
+   match p in (Id a b) return (P a → P b) with
+   | refl _ => id
+   end).
 reflexivity.
 Qed.
 
 (* hott section 1.12.1 *)
 
-Definition inductive_path {A} (C : ∀ (x y : A), Id x y → Type) c x y
-    (p : Id x y) :=
-  match p as p in (Id a b) return (C a b p) with
-  | refl a => c a
-  end.
-
 Theorem path_induction :
   ∀ A,
-  ∀ (C : ∀ x y : A, Id x y → Prop),
-  ∀ (c : ∀ x, C x x (refl x)),
-  ∃ (f : ∀ x y : A, ∀ p : Id x y, C x y p),
+  ∀ (P : ∀ x y : A, Id x y → Prop),
+  ∀ (c : ∀ x, P x x (refl x)),
+  ∃ (f : ∀ x y : A, ∀ p : Id x y, P x y p),
   ∀ x, f x x (refl x) = c x.
 Proof.
-intros A C c.
-exists (inductive_path C c).
+intros A P c.
+exists
+  (λ x y p,
+   match p as p in (Id a b) return (P a b p) with
+   | refl a => c a
+   end).
 reflexivity.
 Qed.
 
