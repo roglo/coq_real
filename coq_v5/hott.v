0c84537b95ce54b01e681cee010508997f01d6fb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-13 22:10:34 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -323,17 +323,19 @@ Definition lift {A P} {x y : A} (u : P x) (p : x == y) :=
      : ∀ (A : Type) (P : A → Type) (x y : A) (u : P x) (p : x == y),
        existT x u == existT y (transport P p u) *)
 
-Check projT1.
+Lemma path_lifting_property : ∀ A P (x y : A) (u : P x) (p : x == y),
+  projT1 (lift u p) == p.
 
-Lemma path_lifting_property : ∀ u p, projT1 (lift u p) == p.
-
-Toplevel input, characters 47-55:
+Toplevel input, characters 103-111:
 Error: In environment
-u : ?1334 ?1335
-p : ?1335 == ?1336
-The term "lift u p" has type
- "existT ?1335 u == existT ?1336 (transport ?1334 p u)"
- while it is expected to have type "sigT ?1327".
+A : Type
+P : A → Type
+x : A
+y : A
+u : P x
+p : x == y
+The term "lift u p" has type "existT x u == existT y (transport P p u)"
+ while it is expected to have type "sigT ?1330".
 
 (* lemma 2.3.4 *)
 
