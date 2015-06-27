7aa083e596da7afd6b3667a80bd7887b2e01337b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-27 17:09:44 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -646,60 +646,19 @@ Id_ind A x (λ (z : A) (q : x == z), q • q ⁻¹ = refl x) eq_refl y p
      : ∀ (A : Type) (x y : A) (p : x == y), p • p ⁻¹ = refl x
 *)
 
-Lemma tete : ∀ p, p == refl 1.
-Proof.
-intros.
-refine (match p with refl => _ end); reflexivity.
-Qed.
-
-Lemma tata : ∀ p, p == refl 0.
-Proof.
-intros.
-refine (match p with refl => _ end); reflexivity.
-Qed.
-
-Lemma toutou : ∀ p, p == refl 2.
-Proof.
-intros.
-refine (match p with refl => _ end).
-Show Proof.
-reflexivity.
-Qed.
-Print toutou.
-
-Theorem f_id {A B} : ∀ x y (f : A → B), x == y → f x == f y.
-Proof.
-intros.
-refine (match H with refl => refl (f x)  end).
-Defined.
-
-Fixpoint pouet n : n == n + 0 :=
-  match n return (n == n + 0) with
-  | 0 => refl (0 + 0)
-  | S n1 => f_id n1 (n1 + 0) S (pouet n1)
+Definition tata (p : 0 == 0) : p == refl 0 :=
+  match p with
+  | refl => refl (refl 0)
   end.
 
-Definition glip m n : m == m + n → Type :=
-  match n return (m == m + n → Type) with
-  | 0 => λ p, p == pouet m
-  | S n1 => λ _, ID
+Definition tete (p : 1 == 1) : p == refl 1 :=
+  match p with
+  | refl => refl (refl 1)
   end.
 
-Definition glup n : 2 == S n → Type :=
-  match n return (2 == S n → Type) with
-  | 0 => λ _ : 2 == 1, ID
-  | S n1 => glip 2 n1
-  end.
-
-Definition glop n : 2 == n → Type :=
-  match n return (2 == n → Type) with
-  | 0 => λ _ : 2 == 0, ID
-  | S n0 => glup n0
-  end.
-
-Definition tratra (p : 2 == 2) : p == refl 2 :=
-  match p in (_ == n) return (glop n p) with
-  | refl => refl (pouet 2)
+Definition toutou (p : 2 == 2) : p == refl 2 :=
+  match p with
+  | refl => refl (refl 2)
   end.
 
 bbb.
