eb565909880d4e3d5562e4965eb25dce416172ea
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-06-28 20:25:10 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -929,6 +929,34 @@ Definition hott_2_3_11 {A x y} : ∀ (P Q : A → U) (f : Π (x : A), P x → Q 
 
 bbb.
 
+Definition homotopy {A : Prop} {B} (f g : A → B) := Π (x : A), (f x = g x).
+
+Notation "f '~~' g" := (homotopy f g) (at level 110, g at level 110).
+
+Theorem homotopy_refl {A B} : reflexive _ (@homotopy A B).
+Proof. intros f x; reflexivity. Qed.
+
+Theorem homotopy_symm {A B} : symmetric _ (@homotopy A B).
+Proof. intros f g H x; symmetry; apply H. Qed.
+
+Theorem homotopy_trans {A B} : transitive _ (@homotopy A B).
+Proof.
+intros f g h Hfg Hgh x.
+transitivity (g x); [ apply Hfg | apply Hgh ].
+Qed.
+
+Add Parametric Relation {A B} : _ (@homotopy A B)
+ reflexivity proved by homotopy_refl
+ symmetry proved by homotopy_symm
+ transitivity proved by homotopy_trans
+ as homotopy_equivalence.
+
+Lemma hott_2_4_3 : ∀ (A : Prop) B (f g : A → B) (H : f ~~ g)
+  (eq_A : A → A → Prop) x y (p : eq_A x y),
+  H x . g p = f p . H y.
+
+bbb.
+
 Lemma toto {A} : ∀ x y : A, ∀ p : x == y, p • p⁻¹ = refl x.
 Proof.
 intros.
@@ -1202,39 +1230,3 @@ Error: Abstracting over the terms "x0" and "p" leads to a term
 refine (
   Id_ind A x (λ (z : A) (q : z == z), q = refl z) eq_refl x p
 ).
-
-bbb.
-
-(* hott, later... *)
-
-Definition pi_type (A : Prop) (B : A → Prop) := ∀ x : A, B x.
-
-Notation "'Π' ( x : A ) , B" :=
-  (pi_type A (λ x, B)) (at level 100, x at level 0, B at level 100).
-
-Definition homotopy {A : Prop} {B} (f g : A → B) := Π (x : A), (f x = g x).
-
-Notation "f '~~' g" := (homotopy f g) (at level 110, g at level 110).
-
-Theorem homotopy_refl {A B} : reflexive _ (@homotopy A B).
-Proof. intros f x; reflexivity. Qed.
-
-Theorem homotopy_symm {A B} : symmetric _ (@homotopy A B).
-Proof. intros f g H x; symmetry; apply H. Qed.
-
-Theorem homotopy_trans {A B} : transitive _ (@homotopy A B).
-Proof.
-intros f g h Hfg Hgh x.
-transitivity (g x); [ apply Hfg | apply Hgh ].
-Qed.
-
-Add Parametric Relation {A B} : _ (@homotopy A B)
- reflexivity proved by homotopy_refl
- symmetry proved by homotopy_symm
- transitivity proved by homotopy_trans
- as homotopy_equivalence.
-
-Lemma hott_2_4_3 : ∀ (A : Prop) B (f g : A → B) (H : f ~~ g)
-  (eq_A : A → A → Prop) x y (p : eq_A x y),
-  H x . g p = f p . H y.
-
