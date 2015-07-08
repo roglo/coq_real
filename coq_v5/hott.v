cce42fffafdaa5f54497b222cfcff25276c804fe
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-07-08 21:18:19 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -1619,10 +1619,11 @@ assert (qinv k) as H2 by apply Hiq, iseq.
 destruct H2 as (g, γ, δ).
 unfold "~~", "o", id in γ, δ.
 assert (∀ e : isequiv k, e == iseq) as He by (intros; apply Hee).
+bbb.
+
 assert (∀ e : qinv k, e == Hiq iseq) as Hq.
  intros.
  destruct e.
-Focus 2.
 
 Print isequiv.
 bbb.
