c2bc2c9688a8001fc29d790d3ab61f66dfd15c55
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-08-16 21:41:06 +0200

    -

--- coq_v5/hott.v
+++ coq_v5/hott.v
@@ -2905,13 +2905,10 @@ Defined.
 
 Theorem toto : ∀ A B C D,
   (A ≃ C)
-  → (Σ (a : A), B a ≃ D)
+  → (∀ (a : A), B a ≃ D)
   → (Σ (a : A), B a) ≃ (C * D).
 Proof.
 intros A B C D HAC HBD.
-(*
-  destruct HBD as (x, (f, Hfg)).
-*)
 unfold equivalence.
 assert ((Σ (a : A), B a) → C * D) as f.
  intros (x, y).
@@ -2919,19 +2916,23 @@ assert ((Σ (a : A), B a) → C * D) as f.
   destruct HAC as (f, Hf).
   apply f, x.
 
-  destruct HBD as (z, (f, Hfg)).
-  apply f.
-bbb.
-(* bin non, ça va pas *)
+  destruct (HBD x) as (f, Hf).
   apply f, y.
 
  apply (existT _ f), qinv_isequiv.
  assert (C * D → (Σ (a : A), B a)) as g.
   intros (x, y).
+  apply quasi_inv in HAC.
   destruct HAC as (g, Hg).
+  apply (existT _ (g x)).
+  pose proof (HBD (g x))⁻⁻¹ as H.
+  destruct H as (h, Hh).
+  apply h, y.
+
+  apply (existT _ g); split; unfold "◦", "~~", id.
+   intros (x, y).
 bbb.
-(* bin non, c'est faux *)
-bbb.
+(* faut le code de f et g *)
 
 Check (pr₂ : Π (x : Semigroup), (_ ◦ pr₁) x).
 
