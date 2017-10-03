0877ce0de63df437fcf659f51c335b2e2db5cd43
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-10-03 11:23:55 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -1,6 +1,6 @@
 (* Real between 0 and 1, i.e. fractional part of a real. *)
 
-Require Import Utf8.
+Require Import Utf8 Arith.
 
 Class radix := { rad : nat; radi : rad ≥ 2 }.
 Record digit {r : radix} := { dig : nat; digi : dig < rad }.
@@ -11,12 +11,7 @@ Delimit Scope digit_scope with D.
 Definition digit_eq {r : radix} (a b : digit) := dig a = dig b.
 Notation "a = b" := (digit_eq a b) : digit_scope.
 
-Theorem Nat_proof_eq_unicity : ∀ (a b : nat) (p q : a = b), p = q.
-Proof.
-intros.
-bbb.
-
-Theorem digit_cmp_eq {r : radix} : ∀ a b, (a = b)%D ↔ a = b.
+Theorem digit_eq_eq {r : radix} : ∀ a b, (a = b)%D ↔ a = b.
 Proof.
 intros.
 split; intros H; [ | now subst ].
@@ -25,3 +20,5 @@ destruct b as (bdig, bdigi).
 unfold digit_eq in H; simpl in H.
 subst bdig.
 f_equal.
+apply le_unique.
+Qed.
