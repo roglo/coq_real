6b96946349293b451644abaa5eac525b82f7a8c1
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-10-03 10:47:15 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -2,6 +2,18 @@
 
 Require Import Utf8.
 
-Class digit := {
+Class radix := { rad : nat; radi : rad ≥ 2 }.
+Record digit {r : radix} := { dig : nat; digi : dig < rad }.
+Record FracReal {r : radix} := { freal : nat → digit }.
 
-Record FracReal := { freal : nat → digit }.
+Definition digit_cmp {r : radix} (a b : digit) := dig a = dig b.
+
+Theorem digit_cmp_eq {r : radix} : ∀ a b, digit_cmp a b ↔ a = b.
+Proof.
+intros.
+split; intros H; [ | now subst ].
+destruct a as (adig, adigi).
+destruct b as (bdig, bdigi).
+unfold digit_cmp in H; simpl in H.
+subst bdig.
+f_equal.
