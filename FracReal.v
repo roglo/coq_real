e1988be32d8ab61654854defe42c49ee498b90fb
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-10-03 14:07:05 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -2,14 +2,16 @@
 
 Require Import Utf8 Arith.
 
+(* Digits *)
+
 Class radix := { rad : nat; radi : rad ≥ 2 }.
 Record digit {r : radix} := { dig : nat; digi : dig < rad }.
-Record FracReal {r : radix} := { freal : nat → digit }.
 
 Delimit Scope digit_scope with D.
 
 Definition digit_eq {r : radix} (a b : digit) := dig a = dig b.
 Notation "a = b" := (digit_eq a b) : digit_scope.
+Notation "a ≠ b" := (¬ digit_eq a b) : digit_scope.
 
 Theorem digit_eq_eq {r : radix} : ∀ a b, (a = b)%D ↔ a = b.
 Proof.
@@ -22,3 +24,16 @@ subst bdig.
 f_equal.
 apply le_unique.
 Qed.
+
+Theorem digit_eq_dec {r : radix} : ∀ a b, {(a = b)%D} + {(a ≠ b)%D}.
+Proof.
+intros.
+destruct (Nat.eq_dec (dig a) (dig b)) as [Hab| Hab]; [ now left | right ].
+intros H.
+apply digit_eq_eq in H; subst b.
+now apply Hab.
+Qed.
+
+(* Frac Real *)
+
+Record FracReal {r : radix} := { freal : nat → digit }.
