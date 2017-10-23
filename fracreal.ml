61a064ba2dc949c460a5ea29601f1818d7d86c92
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-10-23 20:00:18 +0200

    -

--- fracreal.ml
+++ fracreal.ml
@@ -33,3 +33,49 @@ digit_sequence_normalize 10 (fun i -> {dig = if i < 3 then 7 else 9}) 3;
 digit_sequence_normalize 10 (fun i -> {dig = if i < 3 then 7 else 9}) 4;
 - : digit = {dig=0}
 *)
+
+value le_dec a b = a < b.
+value pow a b = truncate (float a ** float b);
+
+type radix = { rad : int }.
+type digit = { dig : int }.
+type fracreal = { freal : int → digit }.
+
+value rad x = x.rad.
+value dig x = x.dig.
+value freal x = x.freal.
+
+value rec summation_aux b len g =
+  match len with
+  | 0 → 0
+  | _ →
+      let len₁ = len - 1 in
+      g b + summation_aux (succ b) len₁ g
+  end.
+
+value summation b e g = summation_aux b (succ e - b) g;
+
+value sequence_mul (a : int → int) (b : int → int) i =
+  summation 0 i (fun j → a j * b (i - j)).
+
+value freal_mul_series (r : radix) a b =
+  sequence_mul (fun i → dig (freal a i)) (fun i → dig (freal b i)).
+
+value mul_test_seq (r : radix) i u k =
+  let n = rad r * (i + k + 2) in
+  if le_dec (pow (rad r) k - 1) (rfrac (A i u n)) then 0 else 1;
+
+value freal_mul_to_seq (r : radix) (a : fracreal) (b : fracreal) i =
+  let u = freal_mul_series r a b in
+  match o_LPO (mul_test_seq i u) with
+  | Inl _ →
+      let n = rad * (i + 2) in
+      (u i + rint (A i u n) + 1) mod rad
+  | Inr (Exist _ j _) →
+      let n = rad * (i + j + 2) in
+      (u i + rint (A i u n)) mod rad
+  end.
+
+value freal_mul (r : radix) (a : fracreal) (b : fracreal) =
+  let u = freal_mul_to_seq a b in
+  {freal i = mkdig r (u i) (freal_mul_to_seq_lt_rad a b i) }.
