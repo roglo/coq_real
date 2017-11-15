48584a6f52549f7de07da779e62e97fe1a15bebe
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-11-15 21:53:02 +0100

    -

--- FracReal.v
+++ FracReal.v
@@ -671,7 +671,17 @@ destruct j.
     intros k Hk.
     rewrite Nat.sub_succ_l; [ simpl; lia | easy ].
 
-  simpl.
+  replace (i + S k) with (S i + k) by lia.
+  destruct j.
+   rewrite Nat.pow_1_r in H; simpl in H.
+   rewrite <- Nat.pow_succ_r in H; [ | lia ].
+   rewrite Nat.mul_assoc, Nat.mul_shuffle0 in H.
+   apply Nat.mul_le_mono_pos_r in H; [ | apply radix_gt_0 ].
+   rewrite <- Nat.pow_succ_r in H; [ | lia ].
+   eapply IHk.
+    replace (S i + S k) with (i + (S (S k))) by lia.
+    fold n.
+(* mon cul *)
 bbb.
   apply Nat.le_trans with
     (m := Σ (k = 0, j), v (i + 1 + k) * rad ^ (j - k))
