cfb387cd76837cb98762cc6cbe3c1f65f7d93983
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-12-12 10:32:28 +0100

    -

--- Xnat2.v
+++ Xnat2.v
@@ -2096,7 +2096,10 @@ destruct al as [| a1].
   rewrite xnatv_add_comm, Hal, xnatv_add_rep_0_l.
   now rewrite nat_of_list_app_rep_0.
 
- +
+ +simpl in Hab; simpl.
+  rewrite Nat.add_assoc, Nat.add_shuffle0; symmetry.
+  rewrite Nat.add_assoc, Nat.add_shuffle0; symmetry.
+  f_equal.
 bbb.
 
 Theorem xnatv_add_eq_compat {r : radix} : 1 < rad → ∀ a b c,
