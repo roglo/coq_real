ebb8afff90551625236f98bb30f80e6f4133131f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2017-12-13 04:04:33 +0100

    -

--- Xnat2.v
+++ Xnat2.v
@@ -311,3 +311,26 @@ intros.
 unfold xnat_mul; simpl.
 now rewrite list_mul_comm.
 Qed.
+
+(* Mmm.... works not: the #iter must be different in each mul *)
+Lemma list_mul_loop_assoc : ∀ al bl cl i n,
+  list_mul_loop i n al (list_mul_loop i n bl cl) =
+  list_mul_loop i n (list_mul_loop i n al bl) cl.
+Proof.
+intros.
+bbb.
+
+Lemma list_mul_assoc : ∀ al bl cl,
+  list_mul al (list_mul bl cl) = list_mul (list_mul al bl) cl.
+Proof.
+intros.
+unfold list_mul.
+apply list_mul_loop_assoc.
+bbb.
+
+Theorem xnat_mul_assoc : ∀ a b c, (a * (b * c))%X = ((a * b) * c)%X.
+Proof.
+intros.
+unfold xnat_mul; simpl; f_equal.
+apply list_mul_assoc.
+Qed.
