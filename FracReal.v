aa1c5b8d9f14eba4d5f748cfaf491beadb4bede3
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-01-31 19:08:38 +0100

    -

--- FracReal.v
+++ FracReal.v
@@ -2308,8 +2308,10 @@ destruct (LPO_fst (is_9_strict_after nxy i)) as [H1| H1].
   I think that
     nA i n (fd2n (freal_normalize x)) = 0   (because of H5)
     nA i n (fd2n x) is 99999                (because of H5 too)
-  and if nA i n (fd2n y) is not 0 => ok
-  but if nA i n (fd2n y) = 0.... problem *)
+  and if nA i n (fd2n y) is not 0 => ok, because the above 999 overflows
+  but if nA i n (fd2n y) = 0.... problem
+
+  See for a possible counter-example *)
 
 ...
  specialize (nA_all_9 radix_gt_0 (d2n (numbers_to_digits u)) i n H1) as H2.
