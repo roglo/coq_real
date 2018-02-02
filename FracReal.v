22fdf1d9813a5c063b45fea919adc995e7ac41ab
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-02-02 21:30:52 +0100

    -

--- FracReal.v
+++ FracReal.v
@@ -2532,6 +2532,11 @@ assert
      rewrite H3 in H1.
      rewrite Nat.mul_add_distr_l in H1.
      rewrite Nat.mul_assoc in H1.
+(*
+enough (s * (s2 - 1) ≥ s * y * rad ^ S k).
+assert (s * (rad ^ S k - 1) * s2 ≤ nA j n u mod s * rad ^ S k) by lia.
+apply Nat.le_trans with (m := s * (rad ^ S k - 1) * s2); [ | easy ].
+*)
 ...
 }
 (* make a lemma with it *)
