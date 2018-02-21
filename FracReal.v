5fcd84991683807462c23572bd55233cbb8cd7f5
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-02-21 10:53:22 +0100

    -

--- FracReal.v
+++ FracReal.v
@@ -3222,11 +3222,6 @@ destruct (LPO_fst (is_9_strict_after nxy i)) as [H1| H1].
          move H7 before H5.
          assert (H8 : ∀ k, fd2n y (i + k + 1) = rad - 1). {
            intros k.
-(*
-           specialize (Hku 0); simpl in Hku.
-           rewrite Nat.add_0_r, <- Hn, <- Hs in Hku.
-           rewrite Nat.mul_1_r, Nat.sub_0_r in Hku.
-*)
            specialize (Hku k).
            move Hku at bottom.
            remember (S k) as sk; simpl in Hku; subst sk.
@@ -3237,6 +3232,22 @@ destruct (LPO_fst (is_9_strict_after nxy i)) as [H1| H1].
            rewrite nA_freal_add_series in Hku.
            specialize (nA_freal_normalize_0 i n2 x H5) as H8.
            rewrite H8, Nat.add_0_l in Hku; clear H8.
+           rewrite Hs2 in Hku.
+           rewrite Nat.mod_small in Hku; [ | apply nA_upper_bound ].
+           assert (H8 : nA i n2 (fd2n y) < rad ^ (S k + (n2 - i - k - 2))). {
+             replace (S k + (n2 - i - k - 2)) with (n2 - i - 1).
+             -apply nA_upper_bound.
+             -ring_simplify.
+              rewrite Hn2.
+              destruct rad; [ easy | simpl; flia ].
+           }
+           specialize (Nat_ge_mul_pred_pow_pow rad (nA i n2 (fd2n y))) as H.
+           remember
+             (nA i n2 (fd2n y) - (rad ^ S k - 1) * rad ^ (n2 - i - k - 2))
+             as t eqn:Ht.
+           specialize (H (S k) (n2 - i - k - 2) t H8 Hku Ht); clear Ht.
+           destruct H as (H9, H10).
+           clear Hku H8.
 
 ...
 
