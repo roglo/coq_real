c30d557a3ba68d2d256dcbbb84cab969dd14509c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-04-10 10:56:56 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -4967,7 +4967,8 @@ specialize (freal_normalized_cases x) as [H1| H1].
   destruct (LPO_fst (is_9_strict_after (freal xy) i)) as
     [H3| H3].
   *specialize (is_9_strict_after_all_9 _ _ H3) as H4; clear H3.
-exfalso.
+(*
+...
 specialize (H2 (max (n - 1) i - i)) as H5.
 specialize (H4 (max (n - 1) i - i)) as H6.
 replace (i + (max (n - 1) i - i)) with (max (n - 1) i) in H5, H6 by flia.
@@ -4984,12 +4985,14 @@ remember (freal_add_series x y) as v eqn:Hv.
 remember (rad * (m + index_A_not_ge v m + 3)) as p eqn:Hp.
 move v before u; move Hv before Hu.
 move p before np; move Hp before Hnp.
+(* claim: H5 is useless
+   claim: v m = x m + y m = rad-1+rad-1 *)
 ...
+*)
    destruct (lt_dec (S (d2n (freal nxy) i)) rad) as [H5| H5].
   --simpl.
     destruct (lt_dec (S (d2n (freal xy) i)) rad) as [H6| H6].
    ++simpl; f_equal.
-
 ...
 intros.
 unfold freal_eq.
