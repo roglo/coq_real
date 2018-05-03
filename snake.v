f92bd1d898a7c670480bf950371b3d96178b9fbc
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-05-03 21:46:09 +0200

    -

--- snake.v
+++ snake.v
@@ -2,7 +2,7 @@
 
 Require Import Utf8.
 
-Record Group_prop {T} (gr_zero : T) (gr_op : T → T → T)  (gr_in : T → Prop) :=
+Record is_group {T} (gr_zero : T) (gr_op : T → T → T)  (gr_in : T → Prop) :=
   { gp_clos : ∀ x y, gr_in x → gr_in y → gr_in (gr_op x y);
     gp_lid : ∀ x, gr_in x → gr_op gr_zero x = x;
     gp_rid : ∀ x, gr_in x → gr_op x gr_zero = x }.
@@ -12,7 +12,7 @@ Record Group :=
     gr_zero : gr_typ;
     gr_op : gr_typ → gr_typ → gr_typ;
     gr_in : gr_typ → Prop;
-    gr_prop : Group_prop gr_zero gr_op gr_in }.
+    gr_prop : is_group gr_zero gr_op gr_in }.
 
 Record HomGr (A B : Group) :=
   { H_app : gr_typ A → gr_typ B;
@@ -23,14 +23,13 @@ Record HomGr (A B : Group) :=
 
 Arguments H_app [A] [B].
 
-...
-
 Inductive Gr0_set := G0 : Gr0_set.
-Theorem Gr0_is_group : is_group Gr0_set G0 (λ _ _, G0) (λ _, True).
+
+Theorem Gr0_prop : is_group G0 (λ _ _ : Gr0_set, G0) (λ _ : Gr0_set, True).
 Proof.
-unfold is_group; simpl.
-split; [ easy | ].
-now split; intros x H; destruct x.
+split; [ easy | | ].
+-now intros x; destruct x.
+-now intros x; destruct x.
 Qed.
 
 Definition Gr0 :=
@@ -38,7 +37,7 @@ Definition Gr0 :=
       gr_zero := G0;
       gr_op := λ _ _, G0;
       gr_in := λ _, True;
-      gr_prop := Gr0_is_group |}.
+      gr_prop := Gr0_prop |}.
 
 Definition is_initial (G : Group) :=
   ∀ H (f g : HomGr G H) (x : gr_typ G), H_app f x = H_app g x.
@@ -61,14 +60,15 @@ split; intros H f g x.
 Qed.
 
 Theorem Im_is_group {G H} (f : HomGr G H) :
-  is_group (gr_typ H) (gr_zero H) (gr_op H) (λ b, ∃ a, H_app f a = b).
+  is_group (gr_zero H) (gr_op H) (λ b, ∃ a, H_app f a = b).
 Proof.
 intros.
-split; [ | split ].
+split.
 -intros y y' (x & Hx) (x' & Hx').
  subst y y'.
  destruct G, H; simpl in *.
  destruct f; simpl in *.
+...
  unfold is_group in *.
  exists (gr_op0 x x').
  apply H_prop0.
