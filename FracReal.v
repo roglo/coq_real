78892f213efe4b275e7801bda6504c0a9ea375f8
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-06-13 11:45:25 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -5158,7 +5158,14 @@ specialize (freal_normalized_cases x) as [H1| H1].
    }
    now rewrite H1, H3.
   *apply Nat.nle_gt in Hni.
-   ...
+   destruct Hwhi as [| Hwhi ]; [ now subst n | ].
+   destruct n; [ easy | ].
+   replace (S n - 1) with n in Hbef, Hwhi by flia.
+   unfold freal_normalize, fd2n; simpl.
+   unfold digit_sequence_normalize.
+   destruct (LPO_fst (is_9_strict_after (freal nxy) i)) as [H1| H1].
+  --specialize (is_9_strict_after_all_9 _ _ H1) as H2; clear H1.
+    ...
  +destruct Hy as (k & Hk).
   ...
 
