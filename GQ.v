fcc5f9e481163b7057a2144db61aa42b760e739a
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-07-06 01:49:09 +0200

    -

--- GQ.v
+++ GQ.v
@@ -11,13 +11,27 @@ Definition GCadd x y :=
   let g := Nat.gcd n d in
   GQmake (Nat.div n g) (Nat.div d g).
 
-(* y a-t-il une fonction qui fait Nat.div x (Nat.gcd x y) ?
-   car c'est toujours divisible ! *)
+Print GCadd.
 
 Require Import Arith.
 
+Theorem Pouet : ∀ x y n d g,
+  n = GQnum x * GQden y + GQnum y * GQnum y
+  → d = GQden x * GQden y
+  → g = Nat.gcd n d
+  → Nat.gcd (Nat.div n g) (Nat.div d g) = 1.
+Proof.
+intros * Hn Hd Hg.
+rewrite Nat.gcd_div_gcd; [ easy | | easy ].
+Search (Nat.gcd (Nat.div _ _)).
+...
+
 Definition div_gcd x y := Nat.div x (Nat.gcd x y).
+
+(* y a-t-il une fonction qui fait Nat.div x (Nat.gcd x y) ?
+   car c'est toujours divisible ! *)
  
 (* mais en un seul coup, sans "vraie" division et sans "vrai" pgcd,
    c'est possible ? *)
 
+Print Nat.gcd.
