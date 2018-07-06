06e2c47aaf8424642841319e80b1e3dbbba4a9ad
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-07-06 10:16:09 +0200

    -

--- GQ.v
+++ GQ.v
@@ -8,25 +8,42 @@ Record GQ :=
     { GQnum1 : nat; GQden1 : nat;
       GQprop : Nat.gcd (S GQnum1) (S GQden1) = 1 }.
 
-Theorem glop : ∀ n d (g := Nat.gcd n d),
+Theorem GCadd_prop : ∀ x y
+  (n := S (GQnum1 x) * S (GQden1 y) + S (GQnum1 y) * S (GQnum1 y))
+  (d := S (GQden1 x) * S (GQden1 y))
+  (g := Nat.gcd n d),
   Nat.gcd (S (n / g - 1)) (S (d / g - 1)) = 1.
 Proof.
-intros *.
+intros.
 rewrite <- Nat.sub_succ_l.
 -rewrite <- Nat.sub_succ_l.
  +do 2 rewrite Nat.sub_succ, Nat.sub_0_r.
   rewrite Nat.gcd_div_gcd; [ easy | | easy ].
   subst g; intros H.
-  apply Nat.gcd_eq_0 in H.
-...
+  now apply Nat.gcd_eq_0 in H.
+ +specialize (Nat.gcd_divide_r n d) as H.
+  fold g in H.
+  unfold Nat.divide in H.
+  destruct H as (c & Hc).
+  rewrite Hc, Nat.div_mul.
+  *destruct c; [ easy | apply Nat.lt_0_succ ].
+  *now intros H; rewrite Nat.mul_comm, H in Hc.
+-specialize (Nat.gcd_divide_l n d) as H.
+ fold g in H.
+ unfold Nat.divide in H.
+ destruct H as (c & Hc).
+ rewrite Hc, Nat.div_mul.
+ *destruct c; [ easy | apply Nat.lt_0_succ ].
+ *now intros H; rewrite Nat.mul_comm, H in Hc.
+Qed.
 
 Definition GCadd x y :=
   let n := S (GQnum1 x) * S (GQden1 y) + S (GQnum1 y) * S (GQnum1 y) in
   let d := S (GQden1 x) * S (GQden1 y) in
   let g := Nat.gcd n d in
-  GQmake (Nat.div n g - 1) (Nat.div d g - 1) (glop n d).
+  GQmake (Nat.div n g - 1) (Nat.div d g - 1) (GCadd_prop x y).
 
-Print GCadd.
+...
 
 Definition div_gcd x y := Nat.div x (Nat.gcd x y).
 
