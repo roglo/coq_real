a61dc9f52b6cdb9f62edfcfdd769ffe40bc40bed
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-07-11 11:41:40 +0200

    -

--- GQ.v
+++ GQ.v
@@ -114,204 +114,11 @@ Notation "x * y" := (GQmul x y) : GQ_scope.
 Notation "x / y" := (GQmul x (GQinv y)) : GQ_scope.
 Notation "/ x" := (GQinv x) : GQ_scope.
 
-(*
-Definition GQN a b := (GQ_of_nat a / GQ_of_nat b)%GQ.
-*)
-Theorem GQN_prop : ∀ a b,
-  Nat.gcd (S (div_gcd_l a b - 1)) (S (div_gcd_r a b - 1)) = 1.
-Proof.
-...
-
-Definition GQN a b := GQmake (div_gcd_l a b - 1) (div_gcd_r a b - 1).
-
-...
-
-Print GQN.
-GQ_of_nat = λ n : nat, {| GQnum1 := n - 1; GQden1 := 0; GQprop := Nat.gcd_1_r (S (n - 1)) |}
-
-(*
-Notation "x +/+ y" := (GQmake x y _) (at level 40, only parsing) : GQ_scope.
-*)
-
-Compute GQN 7 3.
-Compute GQN 16 24.
-Compute GQN 2 4.
-Compute GQN 3 6.
-Compute GQN 4 8.
-
-Theorem GQnum1_make : ∀ n d p, GQnum1 (GQmake n d p) = n.
-Proof. easy. Qed.
-
-Theorem GQden1_make : ∀ n d p, GQden1 (GQmake n d p) = d.
-Proof. easy. Qed.
-
-Theorem GQadd_num_make : ∀ xn xd xp yn yd yp,
-  GQadd_num (GQmake xn xd xp) (GQmake yn yd yp) = S xn * S yd + S yn * S xd.
-Proof. easy. Qed.
-
-Theorem GQadd_den_make : ∀ xn xd xp yn yd yp,
-  GQadd_den (GQmake xn xd xp) (GQmake yn yd yp) = S xd * S yd.
-Proof. easy. Qed.
-
-Theorem GQadd_num_make_l : ∀ xn xd xp y,
-  GQadd_num (GQmake xn xd xp) y = S xn * S (GQden1 y) + S (GQnum1 y) * S xd.
-Proof. easy. Qed.
-
-Theorem GQadd_den_make_l : ∀ n d p y,
-  GQadd_den (GQmake n d p) y = S d * S (GQden1 y).
-Proof. easy. Qed.
-
-Axiom GQeq : ∀ x y, x = y ↔ GQnum1 x = GQnum1 y ∧ GQden1 x = GQden1 y.
-
-Theorem GQadd_comm : ∀ x y, (x + y = y + x)%GQ.
-Proof.
-intros.
-apply GQeq; unfold "+"%GQ.
-do 2 rewrite GQnum1_make, GQden1_make.
-split; f_equal.
--f_equal; [ apply Nat.add_comm | apply Nat.mul_comm ].
--f_equal; [ apply Nat.add_comm | apply Nat.mul_comm ].
-Qed.
-
-Theorem eq_div_gcd_l_1_iff : ∀ a b,
-  a ≠ 0 → div_gcd_l a b = 1 ↔ Nat.divide a b.
-Proof.
-intros * Ha.
-split; intros H.
--exists (b / a).
- unfold div_gcd_l in H.
- specialize (Nat.gcd_divide_l a b) as H1.
- destruct H1 as (c, Hc).
- rewrite Hc in H at 1.
- rewrite Nat.div_mul in H.
- +subst c.
-  rewrite Nat.mul_1_l in Hc.
-  rewrite Hc at 1;
-  rewrite Nat.mul_comm, <- Nat.gcd_div_swap, <- Hc.
-  rewrite Nat.div_same; [ | easy ].
-  now rewrite Nat.mul_1_l.
- +now intros H1; rewrite H1, Nat.mul_0_r in Hc.
--destruct H as (c, Hc); subst b.
- unfold div_gcd_l.
- rewrite Nat.mul_comm, Nat.gcd_mul_diag_l; [ | apply Nat.le_0_l ].
- now apply Nat.div_same.
-Qed.
-
-Theorem eq_div_gcd_l_same_iff : ∀ a b,
-  a ≠ 0 → div_gcd_l a b = a ↔ Nat.gcd a b = 1.
-Proof.
-intros * Ha.
-unfold div_gcd_l.
-split; intros H.
--specialize (Nat.gcd_divide_l a b) as H1.
- destruct H1 as (c, Hc).
- rewrite Hc in H at 1.
- rewrite Nat.div_mul in H.
- +subst c; symmetry in Hc.
-  rewrite <- Nat.mul_1_r in Hc.
-  now apply Nat.mul_cancel_l in Hc.
- +now intros H1; rewrite H1, Nat.mul_0_r in Hc.
--now rewrite H, Nat.div_1_r.
-Qed.
-
 Theorem div_gcd_l_r : ∀ a b, div_gcd_l a b = div_gcd_r b a.
 Proof.
 intros.
 unfold div_gcd_l, div_gcd_r.
 now rewrite Nat.gcd_comm.
-Qed.
-
-Theorem eq_div_gcd_r_same_iff : ∀ a b,
-  b ≠ 0 → div_gcd_r a b = b ↔ Nat.gcd a b = 1.
-Proof.
-intros * Ha.
-split; intros H.
--rewrite Nat.gcd_comm.
- apply eq_div_gcd_l_same_iff; [ easy | ].
- rewrite <- H at 2.
- apply div_gcd_l_r.
--unfold div_gcd_r; rewrite H.
- apply Nat.div_1_r.
-Qed.
-
-Definition GQ_of_PQ x := GQN (S (PQnum1 x)) (S (PQden1 x)).
-Definition PQ_of_GQ x := PQmake (GQnum1 x) (GQden1 x).
-Arguments GQ_of_PQ x%PQ.
-Arguments PQ_of_GQ x%GQ.
-
-Theorem GQ_o_PQ : ∀ x, GQ_of_PQ (PQ_of_GQ x) = x.
-Proof.
-intros (xn, xd, xp).
-apply GQeq.
-unfold GQ_of_PQ, PQ_of_GQ; simpl.
-unfold GQmul_num, GQadd_den, GQ_of_nat; simpl.
-do 2 rewrite Nat.sub_0_r.
-rewrite Nat.add_0_r, Nat.mul_1_r.
-split.
--apply eq_div_gcd_l_same_iff in xp; [ | easy ].
- now rewrite xp, Nat.sub_succ, Nat.sub_0_r.
--apply eq_div_gcd_r_same_iff in xp; [ | easy ].
- now rewrite xp, Nat.sub_succ, Nat.sub_0_r.
-Qed.
-
-Theorem GQadd_PQadd : ∀ x y, (x + y)%GQ = GQ_of_PQ (PQ_of_GQ x + PQ_of_GQ y).
-Proof.
-intros.
-apply GQeq.
-unfold "+"%GQ.
-remember GQ_of_PQ as f; simpl; subst f.
-remember (PQ_of_GQ x + PQ_of_GQ y)%PQ as z.
-simpl; f_equal.
-unfold "/"%GQ; simpl; rewrite Nat.sub_0_r.
-unfold GQmul_num, GQadd_den.
-remember S as f; simpl; subst f.
-rewrite Nat.mul_1_r, Nat.mul_1_l.
-rewrite <- Nat.sub_succ_l; [ | flia ].
-rewrite Nat.sub_succ, Nat.sub_0_r.
-unfold "+"%PQ in Heqz.
-unfold PQadd_num1, PQadd_den1, nd in Heqz.
-simpl in Heqz;  subst z.
-remember S as f; simpl; subst f.
-PQtac1.
-rewrite <- Nat.sub_succ_l; [ | simpl; flia ].
-rewrite Nat.sub_succ, Nat.sub_0_r.
-now rewrite <- Nat.sub_succ_l; [ easy | simpl; flia ].
-Qed.
-
-Theorem GQnum1_of_nat : ∀ m, GQnum1 (GQ_of_nat m) = m - 1.
-Proof. easy. Qed.
-
-Theorem GQden1_of_nat : ∀ m, GQden1 (GQ_of_nat m) = 0.
-Proof. easy. Qed.
-
-Theorem GQnum1_GQN : ∀ n d,
-  GQnum1 (GQN (S n) (S d)) = div_gcd_l (S n) (S d) - 1.
-Proof.
-intros; simpl.
-unfold GQmul_num, GQadd_den; simpl.
-rewrite Nat.mul_1_r, Nat.add_0_r.
-now do 2 rewrite Nat.sub_0_r.
-Qed.
-
-Theorem GQden1_GQN : ∀ n d,
-  GQden1 (GQN (S n) (S d)) = div_gcd_r (S n) (S d) - 1.
-Proof.
-intros; simpl.
-unfold GQmul_num, GQadd_den; simpl.
-rewrite Nat.mul_1_r, Nat.add_0_r.
-now do 2 rewrite Nat.sub_0_r.
-Qed.
-
-Theorem div_gcd_l_succ_l_pos : ∀ n d, 0 < div_gcd_l (S n) d.
-Proof.
-intros.
-unfold div_gcd_l.
-specialize (Nat.gcd_divide_l (S n) d) as (c, Hc).
-rewrite Hc at 1.
-rewrite Nat.div_mul.
--destruct c; [ easy | apply Nat.lt_0_succ ].
--intros H; rewrite H in Hc.
- now rewrite Nat.mul_0_r in Hc.
 Qed.
 
 Fixpoint ggcdn it a b :=
@@ -446,6 +253,238 @@ rewrite <- Hg in H1; simpl in H1.
 rewrite <- H1.
 rewrite Nat.mul_comm, Nat.div_mul; [ easy | ].
 now intros H; subst g.
+Qed.
+
+Theorem ggcd_div_gcd_r : ∀ a b,
+  b ≠ 0 → snd (snd (ggcd a b)) = div_gcd_r a b.
+Proof.
+intros * Hb.
+specialize (ggcd_correct_divisors a b Hb) as H.
+remember (ggcd a b) as g eqn:Hg.
+destruct g as (g, (aa, bb)).
+destruct H as (H1, H2); simpl.
+unfold div_gcd_r.
+subst a b.
+specialize (ggcd_gcd (g * aa) (g * bb)) as H1.
+rewrite <- Hg in H1; simpl in H1.
+rewrite <- H1.
+rewrite Nat.mul_comm, Nat.div_mul; [ easy | ].
+now intros H; subst g.
+Qed.
+
+Theorem div_gcd_l_succ_l_pos : ∀ n d, 0 < div_gcd_l (S n) d.
+Proof.
+intros.
+unfold div_gcd_l.
+specialize (Nat.gcd_divide_l (S n) d) as (c, Hc).
+rewrite Hc at 1.
+rewrite Nat.div_mul.
+-destruct c; [ easy | apply Nat.lt_0_succ ].
+-intros H; rewrite H in Hc.
+ now rewrite Nat.mul_0_r in Hc.
+Qed.
+
+(*
+Definition GQN a b := (GQ_of_nat a / GQ_of_nat b)%GQ.
+*)
+Theorem GQN_prop : ∀ a b,
+  Nat.gcd (S (div_gcd_l a b - 1)) (S (div_gcd_r a b - 1)) = 1.
+Proof.
+intros.
+destruct a; [ now destruct b | ].
+destruct b.
+-remember Nat.gcd as f.
+ unfold div_gcd_l, div_gcd_r; simpl.
+ rewrite Nat.sub_diag.
+ rewrite Nat.div_0_l; [ | easy ].
+ remember Nat.div as g; simpl; subst g.
+ rewrite Nat.div_same; [ now subst f | easy ].
+-rewrite <- Nat.sub_succ_l; [ | apply div_gcd_l_succ_l_pos ].
+ rewrite <- div_gcd_l_r.
+ rewrite <- Nat.sub_succ_l; [ | apply div_gcd_l_succ_l_pos ].
+ do 2 rewrite Nat.sub_succ, Nat.sub_0_r.
+ rewrite <- ggcd_div_gcd_l; [ | easy ].
+ rewrite div_gcd_l_r.
+ rewrite <- ggcd_div_gcd_r; [ | easy ].
+ unfold ggcd; simpl.
+ remember (a ?= b) as c1 eqn:Hc1; symmetry in Hc1.
+ destruct c1; [ easy | | ].
+ +apply Nat.compare_lt_iff in Hc1.
+  remember (ggcdn (a + S b + 1) (b - a) (S a)) as g eqn:Hg.
+  symmetry in Hg.
+  destruct g as (g, (aa, bb)); simpl.
+...
+
+Definition GQN a b := GQmake (div_gcd_l a b - 1) (div_gcd_r a b - 1).
+...
+
+Print GQN.
+GQ_of_nat = λ n : nat, {| GQnum1 := n - 1; GQden1 := 0; GQprop := Nat.gcd_1_r (S (n - 1)) |}
+
+(*
+Notation "x +/+ y" := (GQmake x y _) (at level 40, only parsing) : GQ_scope.
+*)
+
+Compute GQN 7 3.
+Compute GQN 16 24.
+Compute GQN 2 4.
+Compute GQN 3 6.
+Compute GQN 4 8.
+
+Theorem GQnum1_make : ∀ n d p, GQnum1 (GQmake n d p) = n.
+Proof. easy. Qed.
+
+Theorem GQden1_make : ∀ n d p, GQden1 (GQmake n d p) = d.
+Proof. easy. Qed.
+
+Theorem GQadd_num_make : ∀ xn xd xp yn yd yp,
+  GQadd_num (GQmake xn xd xp) (GQmake yn yd yp) = S xn * S yd + S yn * S xd.
+Proof. easy. Qed.
+
+Theorem GQadd_den_make : ∀ xn xd xp yn yd yp,
+  GQadd_den (GQmake xn xd xp) (GQmake yn yd yp) = S xd * S yd.
+Proof. easy. Qed.
+
+Theorem GQadd_num_make_l : ∀ xn xd xp y,
+  GQadd_num (GQmake xn xd xp) y = S xn * S (GQden1 y) + S (GQnum1 y) * S xd.
+Proof. easy. Qed.
+
+Theorem GQadd_den_make_l : ∀ n d p y,
+  GQadd_den (GQmake n d p) y = S d * S (GQden1 y).
+Proof. easy. Qed.
+
+Axiom GQeq : ∀ x y, x = y ↔ GQnum1 x = GQnum1 y ∧ GQden1 x = GQden1 y.
+
+Theorem GQadd_comm : ∀ x y, (x + y = y + x)%GQ.
+Proof.
+intros.
+apply GQeq; unfold "+"%GQ.
+do 2 rewrite GQnum1_make, GQden1_make.
+split; f_equal.
+-f_equal; [ apply Nat.add_comm | apply Nat.mul_comm ].
+-f_equal; [ apply Nat.add_comm | apply Nat.mul_comm ].
+Qed.
+
+Theorem eq_div_gcd_l_1_iff : ∀ a b,
+  a ≠ 0 → div_gcd_l a b = 1 ↔ Nat.divide a b.
+Proof.
+intros * Ha.
+split; intros H.
+-exists (b / a).
+ unfold div_gcd_l in H.
+ specialize (Nat.gcd_divide_l a b) as H1.
+ destruct H1 as (c, Hc).
+ rewrite Hc in H at 1.
+ rewrite Nat.div_mul in H.
+ +subst c.
+  rewrite Nat.mul_1_l in Hc.
+  rewrite Hc at 1;
+  rewrite Nat.mul_comm, <- Nat.gcd_div_swap, <- Hc.
+  rewrite Nat.div_same; [ | easy ].
+  now rewrite Nat.mul_1_l.
+ +now intros H1; rewrite H1, Nat.mul_0_r in Hc.
+-destruct H as (c, Hc); subst b.
+ unfold div_gcd_l.
+ rewrite Nat.mul_comm, Nat.gcd_mul_diag_l; [ | apply Nat.le_0_l ].
+ now apply Nat.div_same.
+Qed.
+
+Theorem eq_div_gcd_l_same_iff : ∀ a b,
+  a ≠ 0 → div_gcd_l a b = a ↔ Nat.gcd a b = 1.
+Proof.
+intros * Ha.
+unfold div_gcd_l.
+split; intros H.
+-specialize (Nat.gcd_divide_l a b) as H1.
+ destruct H1 as (c, Hc).
+ rewrite Hc in H at 1.
+ rewrite Nat.div_mul in H.
+ +subst c; symmetry in Hc.
+  rewrite <- Nat.mul_1_r in Hc.
+  now apply Nat.mul_cancel_l in Hc.
+ +now intros H1; rewrite H1, Nat.mul_0_r in Hc.
+-now rewrite H, Nat.div_1_r.
+Qed.
+
+Theorem eq_div_gcd_r_same_iff : ∀ a b,
+  b ≠ 0 → div_gcd_r a b = b ↔ Nat.gcd a b = 1.
+Proof.
+intros * Ha.
+split; intros H.
+-rewrite Nat.gcd_comm.
+ apply eq_div_gcd_l_same_iff; [ easy | ].
+ rewrite <- H at 2.
+ apply div_gcd_l_r.
+-unfold div_gcd_r; rewrite H.
+ apply Nat.div_1_r.
+Qed.
+
+Definition GQ_of_PQ x := GQN (S (PQnum1 x)) (S (PQden1 x)).
+Definition PQ_of_GQ x := PQmake (GQnum1 x) (GQden1 x).
+Arguments GQ_of_PQ x%PQ.
+Arguments PQ_of_GQ x%GQ.
+
+Theorem GQ_o_PQ : ∀ x, GQ_of_PQ (PQ_of_GQ x) = x.
+Proof.
+intros (xn, xd, xp).
+apply GQeq.
+unfold GQ_of_PQ, PQ_of_GQ; simpl.
+unfold GQmul_num, GQadd_den, GQ_of_nat; simpl.
+do 2 rewrite Nat.sub_0_r.
+rewrite Nat.add_0_r, Nat.mul_1_r.
+split.
+-apply eq_div_gcd_l_same_iff in xp; [ | easy ].
+ now rewrite xp, Nat.sub_succ, Nat.sub_0_r.
+-apply eq_div_gcd_r_same_iff in xp; [ | easy ].
+ now rewrite xp, Nat.sub_succ, Nat.sub_0_r.
+Qed.
+
+Theorem GQadd_PQadd : ∀ x y, (x + y)%GQ = GQ_of_PQ (PQ_of_GQ x + PQ_of_GQ y).
+Proof.
+intros.
+apply GQeq.
+unfold "+"%GQ.
+remember GQ_of_PQ as f; simpl; subst f.
+remember (PQ_of_GQ x + PQ_of_GQ y)%PQ as z.
+simpl; f_equal.
+unfold "/"%GQ; simpl; rewrite Nat.sub_0_r.
+unfold GQmul_num, GQadd_den.
+remember S as f; simpl; subst f.
+rewrite Nat.mul_1_r, Nat.mul_1_l.
+rewrite <- Nat.sub_succ_l; [ | flia ].
+rewrite Nat.sub_succ, Nat.sub_0_r.
+unfold "+"%PQ in Heqz.
+unfold PQadd_num1, PQadd_den1, nd in Heqz.
+simpl in Heqz;  subst z.
+remember S as f; simpl; subst f.
+PQtac1.
+rewrite <- Nat.sub_succ_l; [ | simpl; flia ].
+rewrite Nat.sub_succ, Nat.sub_0_r.
+now rewrite <- Nat.sub_succ_l; [ easy | simpl; flia ].
+Qed.
+
+Theorem GQnum1_of_nat : ∀ m, GQnum1 (GQ_of_nat m) = m - 1.
+Proof. easy. Qed.
+
+Theorem GQden1_of_nat : ∀ m, GQden1 (GQ_of_nat m) = 0.
+Proof. easy. Qed.
+
+Theorem GQnum1_GQN : ∀ n d,
+  GQnum1 (GQN (S n) (S d)) = div_gcd_l (S n) (S d) - 1.
+Proof.
+intros; simpl.
+unfold GQmul_num, GQadd_den; simpl.
+rewrite Nat.mul_1_r, Nat.add_0_r.
+now do 2 rewrite Nat.sub_0_r.
+Qed.
+
+Theorem GQden1_GQN : ∀ n d,
+  GQden1 (GQN (S n) (S d)) = div_gcd_r (S n) (S d) - 1.
+Proof.
+intros; simpl.
+unfold GQmul_num, GQadd_den; simpl.
+rewrite Nat.mul_1_r, Nat.add_0_r.
+now do 2 rewrite Nat.sub_0_r.
 Qed.
 
 (**)
