1f766731c5442470b5c9e6e0640e873a853520b4
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-09-17 20:21:00 +0200

    -

--- ocaml.ml
+++ ocaml.ml
@@ -1,7 +1,7 @@
 (* implementation of reals between 0 and 1 *)
 
 (*
-#load "nums.cma";
+ocaml -I +site-lib/camlp5 camlp5r.cma nums.cma
 *)
 
 open Big_int;
@@ -16,15 +16,24 @@ value rec pow a n =
 
 value rec summation_aux b len g =
   match len with
-  | 0 → zero_big_int
-  | _ → add_big_int (g b) (summation_aux (b + 1) (len - 1) g)
+  | 0 → 0
+  | _ → g b + summation_aux (b + 1) (len - 1) g
   end.
 
 value summation b e g =
   summation_aux b (e + 1 - b) g.
 
+value rec big_int_summation_aux b len g =
+  match len with
+  | 0 → zero_big_int
+  | _ → add_big_int (g b) (big_int_summation_aux (b + 1) (len - 1) g)
+  end.
+
+value big_int_summation b e g =
+  big_int_summation_aux b (e + 1 - b) g.
+
 value nA r i n u =
-  summation (i + 1) (n - 1)
+  big_int_summation (i + 1) (n - 1)
     (fun j → mult_int_big_int (u j) (pow r (n - 1 - j))).
 
 value a_ge_1 r u i k =
@@ -58,8 +67,14 @@ value prop_carr r u i =
   int_of_big_int (mod_big_int d (big_int_of_int r)).
 
 value freal_series_add x y i = x.freal i + y.freal i.
+value freal_series_mul x y i =
+  match i with
+  | 0 → 0
+  | _ → summation 0 (i - 1) (fun j → x.freal j * y.freal (i - 1 - j))
+  end.
 
 value freal_add r x y = { freal = prop_carr r (freal_series_add x y) }.
+value freal_mul r x y = { freal = prop_carr r (freal_series_mul x y) }.
 
 value freal345 =
   {freal i = match i with | 0 → 3 | 1 → 4 | 2 → 5 | _ → 0 end} .
@@ -88,3 +103,39 @@ value frealx = {freal i = if i < 10 then 6 else 0};
 (freal_add 10 freal1_3 frealx).freal 8;
 (freal_add 10 freal1_3 frealx).freal 9;
 (freal_add 10 freal1_3 frealx).freal 10;
+
+value freal1_2 = {freal i = if i = 0 then 5 else 0}.
+(freal_mul 10 freal1_2 freal1_2).freal 0;
+(freal_mul 10 freal1_2 freal1_2).freal 1;
+(freal_mul 10 freal1_2 freal1_2).freal 2;
+freal_series_mul freal1_2 {freal = freal_series_mul freal1_2 freal1_2} 2;
+(* very slow
+(freal_mul 10 freal1_2 (freal_mul 10 freal1_2 freal1_2)).freal 0;
+(freal_mul 10 freal1_2 (freal_mul 10 freal1_2 freal1_2)).freal 1;
+(freal_mul 10 freal1_2 (freal_mul 10 freal1_2 freal1_2)).freal 2;
+*)
+
+value freal1_7 =
+  {freal i =
+     match i mod 6 with
+     | 0 → 1 | 1 → 4 | 2 → 2
+     | 3 → 8 | 4 → 5 | _ → 7
+     end}.
+value freal07 = {freal i = if i = 0 then 7 else 0}.
+(freal_mul 10 freal1_7 freal07).freal 0;
+(freal_mul 10 freal1_7 freal07).freal 1;
+(freal_mul 10 freal1_7 freal07).freal 2;
+value freal02 = {freal i = if i = 0 then 2 else 0}.
+(freal_mul 10 freal1_7 freal02).freal 0;
+(freal_mul 10 freal1_7 freal02).freal 1;
+(freal_mul 10 freal1_7 freal02).freal 2;
+(freal_mul 10 freal1_7 freal02).freal 3;
+(freal_mul 10 freal1_7 freal02).freal 4;
+(freal_mul 10 freal1_7 freal02).freal 5;
+(freal_mul 10 freal1_7 freal02).freal 6;
+(freal_mul 10 freal1_7 freal02).freal 7;
+(freal_mul 10 freal1_7 freal02).freal 8;
+(freal_mul 10 freal1_7 freal02).freal 9;
+(freal_mul 10 freal1_7 freal02).freal 10;
+(freal_mul 10 freal1_7 freal02).freal 11;
+(freal_mul 10 freal1_7 freal02).freal 12;
