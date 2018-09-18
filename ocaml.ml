5589b1dbc06dfc456351f7ef7ad09110e63c74f3
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-09-18 10:28:30 +0200

    -

--- ocaml.ml
+++ ocaml.ml
@@ -6,39 +6,45 @@ ocaml -I +site-lib/camlp5 camlp5r.cma nums.cma
 
 open Big_int;
 
+value lpo_max = 20;
+
 type real01 = { freal : int → int; freal_comp : Hashtbl.t int int }.
 
-value lpo_max = 20;
 value max_i = ref 0;
 
 value make_real f = {freal = f; freal_comp = Hashtbl.create 1}.
 value real_val x i =
+(*
+  x.freal i.
+*)
   try Hashtbl.find x.freal_comp i with
-  [ Not_found → do {
-let _ = max_i.val := max i max_i.val in
+  | Not_found → do {
+      max_i.val := max i max_i.val;
       let v = x.freal i in
       Hashtbl.add x.freal_comp i v;
       v
-    } ].
+    }
+  end.
+(**)
+
+value rec list_seq start len =
+  if len ≤ 0 then []
+  else [start :: list_seq (start + 1) (len - 1)].
 
 value rec pow a n =
-   if n = 0 then unit_big_int
+   if n ≤ 0 then unit_big_int
    else mult_int_big_int a (pow a (n - 1)).
 
 value rec summation_aux b len g =
-  match len with
-  | 0 → 0
-  | _ → g b + summation_aux (b + 1) (len - 1) g
-  end.
+  if len ≤ 0 then 0
+  else g b + summation_aux (b + 1) (len - 1) g.
 
 value summation b e g =
   summation_aux b (e + 1 - b) g.
 
 value rec big_int_summation_aux b len g =
-  match len with
-  | 0 → zero_big_int
-  | _ → add_big_int (g b) (big_int_summation_aux (b + 1) (len - 1) g)
-  end.
+  if len ≤ 0 then zero_big_int
+  else add_big_int (g b) (big_int_summation_aux (b + 1) (len - 1) g).
 
 value big_int_summation b e g =
   big_int_summation_aux b (e + 1 - b) g.
@@ -96,47 +102,30 @@ value freal_add r x y =
 value freal_mul r x y =
   make_real (prop_carr r (freal_series_mul x y));
 
+value real_val_n n x =
+  List.map (real_val x) (list_seq 0 n).
+
 value freal345 =
   make_real (fun i → match i with | 0 → 3 | 1 → 4 | 2 → 5 | _ → 0 end);
 value freal817 =
   make_real (fun i → match i with | 0 → 8 | 1 → 1 | 2 → 7 | _ → 0 end).
-
-real_val (freal_add 10 freal345 freal817) 0;
-real_val (freal_add 10 freal345 freal817) 1;
-real_val (freal_add 10 freal345 freal817) 2;
-real_val (freal_add 10 freal345 freal817) 3;
+real_val_n 5 freal345;
+real_val_n 5 freal817;
+real_val_n 5 (freal_add 10 freal345 freal817);
 
 value freal1_3 = make_real (fun i → 3);
 value freal1_6 = make_real (fun i → 6);
-real_val (freal_add 10 freal1_3 freal1_6) 0;
-real_val (freal_add 10 freal1_3 freal1_6) 1;
+real_val_n 10 freal1_3;
+real_val_n 10 freal1_6;
+real_val_n 10 (freal_add 10 freal1_3 freal1_6);
 
 value frealx = make_real (fun i → if i < 10 then 6 else 0).
-real_val (freal_add 10 freal1_3 frealx) 0;
-real_val (freal_add 10 freal1_3 frealx) 1;
-real_val (freal_add 10 freal1_3 frealx) 2;
-real_val (freal_add 10 freal1_3 frealx) 3;
-real_val (freal_add 10 freal1_3 frealx) 4;
-real_val (freal_add 10 freal1_3 frealx) 5;
-real_val (freal_add 10 freal1_3 frealx) 6;
-real_val (freal_add 10 freal1_3 frealx) 7;
-real_val (freal_add 10 freal1_3 frealx) 8;
-real_val (freal_add 10 freal1_3 frealx) 9;
-real_val (freal_add 10 freal1_3 frealx) 10;
+real_val_n 13 freal1_3;
+real_val_n 13 frealx;
+real_val_n 13 (freal_add 10 freal1_3 frealx);
 
 value freal1_2 = make_real (fun i → if i = 0 then 5 else 0).
-real_val (freal_mul 10 freal1_2 freal1_2) 0;
-real_val (freal_mul 10 freal1_2 freal1_2) 1;
-real_val (freal_mul 10 freal1_2 freal1_2) 2;
-(* slow *)
-value x = freal_mul 10 freal1_2 freal1_2.
-value y = freal_mul 10 freal1_2 x;
-real_val y 0;
-real_val y 1;
-real_val y 2;
-real_val y 3;
-real_val y 4;
-(* *)
+real_val_n 3 (freal_mul 10 freal1_2 freal1_2).
 
 value freal1_7 =
   make_real
@@ -146,20 +135,10 @@ value freal1_7 =
      | 3 → 8 | 4 → 5 | _ → 7
      end).
 value freal07 = make_real (fun i → if i = 0 then 7 else 0).
-real_val (freal_mul 10 freal1_7 freal07) 0;
-real_val (freal_mul 10 freal1_7 freal07) 1;
-real_val (freal_mul 10 freal1_7 freal07) 2;
-value freal02 = make_real (fun i → if i = 0 then 2 else 0).
-real_val (freal_mul 10 freal1_7 freal02) 0;
-real_val (freal_mul 10 freal1_7 freal02) 1;
-real_val (freal_mul 10 freal1_7 freal02) 2;
-real_val (freal_mul 10 freal1_7 freal02) 3;
-real_val (freal_mul 10 freal1_7 freal02) 4;
-real_val (freal_mul 10 freal1_7 freal02) 5;
-real_val (freal_mul 10 freal1_7 freal02) 6;
-real_val (freal_mul 10 freal1_7 freal02) 7;
-real_val (freal_mul 10 freal1_7 freal02) 8;
-real_val (freal_mul 10 freal1_7 freal02) 9;
-real_val (freal_mul 10 freal1_7 freal02) 10;
-real_val (freal_mul 10 freal1_7 freal02) 11;
-real_val (freal_mul 10 freal1_7 freal02) 12;
+real_val_n 8 freal1_7;
+real_val_n 8 freal07;
+real_val_n 8 (freal_mul 10 freal1_7 freal07);
+value freal0n n = make_real (fun i → if i = 0 then n else 0).
+real_val_n 12 freal1_7;
+real_val_n 12 (freal0n 2);
+real_val_n 12 (freal_mul 10 freal1_7 (freal0n 2));
