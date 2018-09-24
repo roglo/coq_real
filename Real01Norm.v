daf7e4e1592ac39ff47237f7487868d5814b27f8
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-09-24 17:32:18 +0200

    -

--- Real01Norm.v
+++ Real01Norm.v
@@ -623,15 +623,12 @@ unfold d2n, prop_carr in H1; simpl in H1.
 rewrite Nat.add_0_r in H1.
 unfold nat_prop_carr in H1.
 destruct (LPO_fst (A_ge_1 u i)) as [H2| H2]; [ apply H2 | ].
+exfalso; (* à mon avis *)
 destruct H2 as (j & Hjj & Hj).
-destruct (lt_dec k j) as [H2| H2]; [ now apply Hjj | ].
-apply Nat.nlt_ge in H2.
-(*
 apply A_ge_1_false_iff in Hj.
 remember (min_n i j) as n eqn:Hn.
 remember (n - i - 1) as s eqn:Hs.
 move s before n.
-*)
 ...
 
 Theorem eq_all_prop_carr_9 {r : radix} : ∀ u n,
