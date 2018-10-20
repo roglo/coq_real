387f4cdfa0ac47bd2a0ba553203195709da037e7
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-10-20 10:54:58 +0200

    -

--- FracReal.v
+++ FracReal.v
@@ -1796,6 +1796,28 @@ specialize radix_ge_2 as Hr.
 rewrite A_split with (e := i + k + 2); [ | flia Hin ].
 remember (i + k + 2) as j eqn:Hj.
 move j before i.
+replace (1 - 1 // rad ^ (k + 2))%NQ with
+   ((rad ^ (k + 1) - 2) // rad ^ (k + 1) +
+    (2 * rad - 1) // rad ^ (k + 2))%NQ; cycle 1. {
+...
+  replace (s - (k + 1)) with (s - (k + 2) + 1) by flia Hs Hj Hin.
+  remember (s - (k + 2)) as t eqn:Ht.
+  replace (k + 2) with (k + 1 + 1) by flia.
+  remember (k + 1) as m eqn:Hm.
+  do 2 rewrite Nat.pow_add_r.
+  rewrite Nat.pow_1_r.
+  rewrite Nat.mul_assoc, Nat.mul_shuffle0.
+  rewrite <- Nat.mul_add_distr_r; f_equal.
+  rewrite Nat.mul_sub_distr_r.
+  rewrite Nat.add_sub_assoc; [ f_equal | flia Hr ].
+  rewrite Nat.sub_add; [ easy | ].
+  apply Nat.mul_le_mono_r.
+  subst m.
+  rewrite Nat.add_comm; simpl.
+  replace 2 with (2 * 1) by flia.
+  apply Nat.mul_le_mono; [ easy | ].
+  apply Nat.neq_0_lt_0; pauto.
+}
 ...
 replace ((rad ^ (k + 2) - 1) * rad ^ (s - (k + 2))) with
    ((rad ^ (k + 1) - 2) * rad ^ (s - (k + 1)) +
