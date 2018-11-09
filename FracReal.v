56f344b0eb92ba7c40bf3fc11a5dfe40ab4f46b0
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2018-11-09 11:48:31 +0100

    -

--- FracReal.v
+++ FracReal.v
@@ -3625,6 +3625,7 @@ specialize (A_upper_bound_for_add u i n Hur) as H1.
 remember (n - i - 1) as s eqn:Hs.
 remember (A i n u) as x eqn:Hx in H1.
 rewrite NQintg_frac in Hx; [ subst x | apply A_ge_0 ].
+remember (NQintg (A i n u)) as x eqn:Hx.
 ...
 Qed.
 (*
