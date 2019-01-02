f8a4ab37af7fffde15cf84bcdff122bda8afc75c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-01-02 09:49:01 +0100

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -1065,7 +1065,9 @@ destruct (LPO_fst (fA_ge_1_ε (u ⊕ P v) i)) as [H1| H1].
 (* d'après H3, A i n (P v) n'a que des 9 ; donc, d'après AA1, u n'a pas
    que des 0 entre i+1 et n-1, sinon ça ne serait pas assez pour dépasser
    1. Donc v ne peut pas avoir que des 9, ça contradirait AA3. Du coup,
-   comment est v ? P(v) est 999... mais pas v. *)
+   comment est v ? P(v) est 999... mais pas v. D'après H'3, v est supérieur
+   ou égal à 9000... Donc, d'après AA3, u est inférieur à 1000... *)
+...
 (* Si on considère que v est
    le résultat d'une addition, alors ayant déjà prouvé que v ne peut être
    que 999...9998/18/18... ou 18/18/18.., je pourrais m'en sortir, mais
