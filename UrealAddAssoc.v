89526124b147b411aa73c05be6ae7fecb10e09cd
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-02-11 17:51:13 +0100

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -3791,24 +3791,58 @@ destruct (LPO_fst (fA_ge_1_ε v i)) as [H3| H3].
     apply NQadd_le_mono_l.
     rewrite <- Hnv, <- Hnuv, NQadd_comm.
     specialize (all_fA_ge_1_ε_P_999 _ _ H3) as A3.
-(* Hm et A3 : soit v = 999... soit v = 999...8/18/18... *)
-...
-    rewrite (A_all_9 (P v)); [ | intros; apply A3 ].
-...
-    apply NQadd_le_mono.
-   ++replace kuv with (0 + kuv) by easy.
-     rewrite min_n_add.
-     rewrite <- ApB_A; [ | easy ].
-     apply NQle_add_r, B_ge_0.
-   ++idtac.
-...
-   assert (H : ∀ n, NQintg (A i n u) = 0). {
-     intros n; apply NQintg_A_for_dig; intros k Hk.
-     replace k with (i + (k - i)) by flia Hk; apply Hu.
-   }
-...
-   specialize (all_fA_ge_1_ε_P_999 _ _ H3) as A3.
-   rewrite (A_all_9 (P v)); [ | intros; apply A3 ].
+    (* Hm et A3 : soit v = 999... soit v = 999...8/18/18... *)
+    (* si 999... c'est bon
+       si 999...8/18/18... c'est plus compliqué, faut voir avec kuv *)
+    specialize (A_ge_1_add_all_true_if v i) as H4.
+    assert (H : ∀ k, v (i + k + 1) ≤ 2 * (rad - 1)). {
+      intros k; rewrite <- Nat.add_assoc; apply Hv.
+    }
+    specialize (H4 H H3); clear H.
+    destruct H4 as [H4| [H4| H4]].
+   ++rewrite A_all_9; [ | easy ].
+     rewrite A_all_9; [ | easy ].
+     eapply NQle_trans; [ | apply NQle_add_r, B_ge_0 ].
+     apply NQsub_le_mono; [ apply NQle_refl | ].
+     apply NQle_pair; [ pauto | pauto | ].
+     rewrite Nat.mul_1_r, Nat.mul_1_l.
+     apply Nat.pow_le_mono_r; [ easy | ].
+     rewrite Hnv, Hnuv.
+     replace kuv with (0 + kuv) by easy.
+     rewrite min_n_add; flia.
+   ++exfalso; apply NQnle_gt in H5; apply H5; clear H5.
+     eapply NQle_trans; [ | now apply NQle_add_l ].
+     rewrite A_all_18; [ | easy ].
+     apply NQle_add_le_sub_l.
+     replace 2%NQ with (1 + 1)%NQ by easy.
+     apply NQadd_le_mono_l.
+     apply NQle_pair; [ pauto | easy | ].
+     do 2 rewrite Nat.mul_1_r.
+     remember (nuv - i - 1) as p eqn:Hp.
+     destruct p. {
+       rewrite Hnuv in Hp; unfold min_n in Hp.
+       destruct rad; [ easy | cbn in Hp; flia Hp ].
+     }
+     cbn; replace 2 with (2 * 1) by easy.
+     apply Nat.mul_le_mono; [ easy | ].
+     now apply Nat_pow_ge_1.
+   ++destruct H4 as (j & Hbef & Hwhi & Haft).
+     destruct (lt_dec nuv (i + j + 2)) as [H4| H4].
+    **rewrite A_all_9; [ | easy ].
+      rewrite A_all_9; [ | intros k Hk; apply Hbef; flia H4 Hk ].
+      eapply NQle_trans; [ | apply NQle_add_r, B_ge_0 ].
+      apply NQsub_le_mono; [ apply NQle_refl | ].
+      apply NQle_pair; [ pauto | pauto | ].
+      rewrite Nat.mul_1_r, Nat.mul_1_l.
+      apply Nat.pow_le_mono_r; [ easy | ].
+      rewrite Hnv, Hnuv.
+      replace kuv with (0 + kuv) by easy.
+      rewrite min_n_add; flia.
+    **apply Nat.nlt_ge in H4.
+      destruct (LPO_fst (fA_ge_1_ε (u ⊕ v) i)) as [H6| H6].
+    ---specialize (A_ge_1_add_all_true_if (u ⊕ v) i) as H7.
+(* ah mais oui mais non... u+v ça va jusqu'à 27, faut que j'utilise
+   mon tout beau théorème général, avec sept cas *)
 ...
 
 Theorem Hugo_Herbelin {r : radix} : ∀ u v i,
