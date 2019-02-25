259f3d2a49c6f75b6e9516f7471e179c92a402bc
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-02-25 19:51:30 +0100

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -5195,112 +5195,80 @@ destruct (NQlt_le_dec (A i nij u + 1 - 1 // rad ^ sij)%NQ 1) as [Hau1| Hau1].
    move Hau1 at bottom.
 clear H1 Hpi.
 exfalso.
-  specialize (Hauv (j + 1)) as H1.
-  rewrite A_additive in H1.
-  rewrite (A_all_18 v) in H1; [ | easy ].
-  rewrite min_n_add, Nat.mul_1_r, <- Hnij in H1.
-  rewrite <- Nat.sub_add_distr in H1.
-  assert (Hinij : i + 1 ≤ nij). {
-    rewrite Hnij; unfold min_n.
-    destruct rad; [ easy | cbn; flia ].
-  }
-  rewrite Nat.add_sub_swap in H1; [ | easy ].
-  rewrite Nat.sub_add_distr, <- Hsij in H1.
-  rewrite <- ApB_A in H1; [ | easy ].
-  rewrite Hau1 in H1.
-  apply NQnlt_ge in H1; apply H1; clear H1.
-  rewrite NQadd_comm.
-  rewrite NQadd_assoc.
-  rewrite <- NQadd_sub_swap.
-  rewrite <- NQadd_sub_assoc.
-  rewrite <- NQadd_assoc.
-  rewrite Nat.pow_add_r.
-  replace (1 // rad ^ sij)%NQ with (1 // rad ^ sij * 1)%NQ at 1
-    by apply NQmul_1_r.
-  replace 2 with (1 * 2) at 2 by easy.
-  rewrite <- NQmul_pair; [ | pauto | pauto ].
-  rewrite <- NQmul_sub_distr_l.
-  rewrite NQfrac_add_nat_l. 2: {
-    replace 0%NQ with (0 * 0 + 0)%NQ by easy.
-    apply NQadd_le_mono; [ | apply B_ge_0 ].
-    apply NQmul_le_mono_nonneg; [ easy | easy | easy | ].
-    now apply H012r.
-  }
-  specialize (B_upper_bound_for_adds 1 u i j rad) as H1.
-  rewrite Nat.mul_1_l, <- Hnij in H1.
-  assert (H : 0 < 1 ≤ rad ^ 2). {
-    split; [ pauto | now apply Nat_pow_ge_1 ].
-  }
-  specialize (H1 H); clear H.
-  assert (H : ∀ j : nat, j ≥ i → u j ≤ rad - 1). {
-    intros p Hp; replace p with (i + (p - i)) by flia Hp; apply Hu.
-  }
-  specialize (H1 H); clear H.
-  rewrite NQfrac_small. 2: {
-    split.
-    -replace 0%NQ with (0 * 0 + 0)%NQ by easy.
+   specialize (Hauv (j + 1)) as H1.
+   rewrite A_additive in H1.
+   rewrite (A_all_18 v) in H1; [ | easy ].
+   rewrite min_n_add, Nat.mul_1_r, <- Hnij in H1.
+   rewrite <- Nat.sub_add_distr in H1.
+   assert (Hinij : i + 1 ≤ nij). {
+     rewrite Hnij; unfold min_n.
+     destruct rad; [ easy | cbn; flia ].
+   }
+   rewrite Nat.add_sub_swap in H1; [ | easy ].
+   rewrite Nat.sub_add_distr, <- Hsij in H1.
+   rewrite <- ApB_A in H1; [ | easy ].
+   rewrite Hau1 in H1.
+   apply NQnlt_ge in H1; apply H1; clear H1.
+   rewrite NQadd_comm.
+   rewrite NQadd_assoc.
+   rewrite <- NQadd_sub_swap.
+   rewrite <- NQadd_sub_assoc.
+   rewrite <- NQadd_assoc.
+   rewrite Nat.pow_add_r.
+   replace (1 // rad ^ sij)%NQ with (1 // rad ^ sij * 1)%NQ at 1
+     by apply NQmul_1_r.
+   replace 2 with (1 * 2) at 2 by easy.
+   rewrite <- NQmul_pair; [ | pauto | pauto ].
+   rewrite <- NQmul_sub_distr_l.
+   rewrite NQfrac_add_nat_l. 2: {
+     replace 0%NQ with (0 * 0 + 0)%NQ by easy.
      apply NQadd_le_mono; [ | apply B_ge_0 ].
      apply NQmul_le_mono_nonneg; [ easy | easy | easy | ].
-     apply NQle_add_le_sub_r; rewrite NQadd_0_r.
-     apply NQle_pair; [ pauto | easy | ].
-     apply Nat.mul_le_mono_r.
-     replace 2 with (2 ^ 1) by easy.
-     now apply Nat.pow_le_mono.
-    -eapply NQlt_le_trans; [ apply NQadd_lt_mono_l, H1 | ].
-     eapply NQle_trans.
-     +apply NQadd_le_mono_r.
-      apply NQmul_le_mono_nonneg with (t := 1%NQ); [ easy | | | ].
-      *apply NQle_refl.
-      *now apply H012r.
-      *now apply NQle_sub_l.
-     +rewrite NQmul_1_r.
-      rewrite NQadd_pair; [ | pauto | pauto ].
-      rewrite Nat.mul_1_l, Nat.mul_1_r.
-      apply NQle_pair; [ | easy | ].
-      *apply Nat.neq_mul_0; pauto.
-      *apply Nat.mul_le_mono_r.
-       rewrite Nat.add_comm.
-       apply Nat.add_le_mul.
-      --rewrite Hnij in Hsij; unfold min_n in Hsij.
-        replace 1 with (1 ^ sij) by apply Nat.pow_1_l.
-        apply Nat.pow_lt_mono_l; [ | easy ].
-        intros H; rewrite H in Hsij.
-        destruct rad; [ easy | cbn in Hsij; flia Hsij ].
-      --replace 1 with (1 ^ S j) by apply Nat.pow_1_l.
-        now apply Nat.pow_lt_mono_l.
-  }
-...
-apply NQlt_sub_lt_add_r.
-rewrite <- NQadd_sub_swap.
-apply -> NQlt_add_lt_sub_r.
-specialize (B_upper_bound_for_adds 1 u i j rad) as H1.
-rewrite Nat.mul_1_l, <- Hnij in H1.
-assert (H : 0 < 1 ≤ rad ^ 2) by admit.
-specialize (H1 H); clear H.
-assert (H : ∀ j : nat, j ≥ i → u j ≤ rad - 1) by admit.
-specialize (H1 H); clear H.
-apply (NQlt_le_trans _ (1 // rad ^ sij + 1 // rad ^ S j + 1 // rad ^ (S (j + 1)))%NQ).
-now apply NQadd_lt_mono_r, NQadd_lt_mono_l.
-rewrite <- NQadd_assoc.
-rewrite NQadd_pair; [ | pauto | pauto ].
-rewrite Nat.mul_1_l, Nat.mul_1_r.
-replace (S (j + 1)) with (S (S j)) by flia.
-rewrite Nat.pow_succ_r' at 1.
-replace (rad ^ S j) with (1 * rad ^ S j) at 2 by flia.
-rewrite <- Nat.mul_add_distr_r.
-rewrite Nat.mul_comm.
-rewrite NQmul_pair_mono_l; [ | pauto | pauto ].
-rewrite NQadd_pair; [ | pauto | pauto ].
-rewrite Nat.mul_1_l.
-rewrite NQadd_pair; [ | easy | pauto ].
-do 2 rewrite Nat.mul_1_l.
-apply NQle_pair; [ | pauto | ]. admit.
-rewrite Nat.mul_add_distr_r.
-do 2 rewrite Nat.mul_add_distr_l.
-rewrite Nat.mul_add_distr_r.
-rewrite Nat.mul_1_r, Nat.add_assoc.
-(* pas sûr que ça soit bon, ça *)
-(* à vérifier *)
+     now apply H012r.
+   }
+   specialize (B_upper_bound_for_adds 1 u i j rad) as H1.
+   rewrite Nat.mul_1_l, <- Hnij in H1.
+   assert (H : 0 < 1 ≤ rad ^ 2). {
+     split; [ pauto | now apply Nat_pow_ge_1 ].
+   }
+   specialize (H1 H); clear H.
+   assert (H : ∀ j : nat, j ≥ i → u j ≤ rad - 1). {
+     intros p Hp; replace p with (i + (p - i)) by flia Hp; apply Hu.
+   }
+   specialize (H1 H); clear H.
+   rewrite NQfrac_small. 2: {
+     split.
+     -replace 0%NQ with (0 * 0 + 0)%NQ by easy.
+      apply NQadd_le_mono; [ | apply B_ge_0 ].
+      apply NQmul_le_mono_nonneg; [ easy | easy | easy | ].
+      apply NQle_add_le_sub_r; rewrite NQadd_0_r.
+      apply NQle_pair; [ pauto | easy | ].
+      apply Nat.mul_le_mono_r.
+      replace 2 with (2 ^ 1) by easy.
+      now apply Nat.pow_le_mono.
+     -eapply NQlt_le_trans; [ apply NQadd_lt_mono_l, H1 | ].
+      eapply NQle_trans.
+      +apply NQadd_le_mono_r.
+       apply NQmul_le_mono_nonneg with (t := 1%NQ); [ easy | | | ].
+       *apply NQle_refl.
+       *now apply H012r.
+       *now apply NQle_sub_l.
+      +rewrite NQmul_1_r.
+       rewrite NQadd_pair; [ | pauto | pauto ].
+       rewrite Nat.mul_1_l, Nat.mul_1_r.
+       apply NQle_pair; [ | easy | ].
+       *apply Nat.neq_mul_0; pauto.
+       *apply Nat.mul_le_mono_r.
+        rewrite Nat.add_comm.
+        apply Nat.add_le_mul.
+        --rewrite Hnij in Hsij; unfold min_n in Hsij.
+          replace 1 with (1 ^ sij) by apply Nat.pow_1_l.
+          apply Nat.pow_lt_mono_l; [ | easy ].
+          intros H; rewrite H in Hsij.
+          destruct rad; [ easy | cbn in Hsij; flia Hsij ].
+        --replace 1 with (1 ^ S j) by apply Nat.pow_1_l.
+          now apply Nat.pow_lt_mono_l.
+   }
 ...
   rewrite NQfrac_add_cond in H1; [ | easy | easy ].
   rewrite NQfrac_small in H1. 2: {
