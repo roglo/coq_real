3b9706e4f0590204da0c17ee6b7a5f0527b56806
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-02-26 11:05:36 +0100

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -5292,8 +5292,8 @@ destruct (NQlt_le_dec (A i nij u + 1 - 1 // rad ^ sij)%NQ 1) as [Hau1| Hau1].
    rewrite NQadd_sub_assoc in Haa.
    apply NQlt_sub_lt_add_l, NQadd_lt_mono_r in Haa.
    move Haa at bottom; move H2 at bottom.
+   (* ah la la, c'est compliqué *)
 ...
-
    rewrite NQsub_sub_swap, NQadd_sub in H1.
    rename H1 into H3.
 ...
