a623b5a4013d76f6e7bb1e6b7c7e42c149ffd8b9
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-04-04 20:47:49 +0200

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -2018,6 +2018,52 @@ clear Hzp1.
 rewrite Hp1.
 replace (1 - 1 // 2)%Q with (1 * 1 // 2)%Q by easy.
 apply Q.mul_lt_mono_pos_r; [ easy | ].
+rewrite A_split_first; [ | min_n_ge ].
+replace (S (i + 1)) with (i + 2) by easy.
+apply Nat_eq_add_2 in Huv2.
+destruct Huv2 as [(Hu2, Hv2)| Huv2]. {
+  exfalso.
+  unfold P, d2n, prop_carr, dig in Hp1.
+  rewrite Hv1, Nat.add_0_l in Hp1.
+  rewrite Nat.mod_small in Hp1. 2: {
+    rewrite Hr2 in Hp1 |-*.
+    remember (carry v (i + 1)) as c1 eqn:Hc1.
+    symmetry in Hc1.
+    destruct c1; [ easy | ].
+    destruct c1; [ pauto | exfalso ].
+    destruct c1; [ easy | ].
+    specialize (carry_upper_bound_for_adds 2 v i) as H1.
+    assert (H : 2 ≠ 0) by easy.
+    specialize (H1 H); clear H.
+    assert (H : ∀ k, v (i + k + 1) ≤ 2 * (rad - 1)). {
+      intros; rewrite <- Nat.add_assoc, Hr2; apply Hv.
+    }
+    specialize (H1 H 1); clear H.
+    rewrite Hc1 in H1; flia H1.
+  }
+  rename Hp1 into Hc1.
+  unfold carry in Hc1.
+  rewrite A_split_first in Hc1; [ | min_n_ge ].
+  replace (S (i + 1)) with (i + 2) in Hc1 by flia.
+  rewrite Hv2, Q.add_0_l in Hc1.
+  apply Q.intg_interv in Hc1; [ | now apply Q.le_0_mul_r ].
+  destruct Hc1 as (Hc1, _).
+  apply Q.nlt_ge in Hc1.
+  exfalso; apply Hc1; clear Hc1; rewrite Hr2.
+  apply (Q.mul_lt_mono_pos_r 2%Q); [ easy | ].
+  rewrite <- Q.mul_assoc.
+  rewrite (Q.mul_pair_den_num _ 2 1); [ | easy ].
+  rewrite Q.pair_diag; [ | easy ].
+  rewrite Q.mul_1_r, Q.mul_1_l.
+  eapply Q.le_lt_trans. {
+    apply (A_upper_bound_for_adds 2).
+    now intros; do 2 rewrite <- Nat.add_assoc; rewrite Hr2.
+  }
+  rewrite Q.mul_sub_distr_l, Q.mul_1_r.
+  now apply Q.sub_lt.
+}
+...
+
 Check rad_2_sum_3_213c1_A_lt_1.
 ...
 
