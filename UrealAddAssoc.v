828c55e2796c5cbd0502fbf25d783477c447ade0
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-05-13 01:25:27 +0200

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -4352,6 +4352,7 @@ destruct (Q.lt_le_dec (A i nk u + Q.frac (A i nk v)) 1) as [H5| H5].
      flia H3 H7 H8.
    }
    destruct Huv789 as [Huv1| Huv1]. {
+...
      destruct (Nat.eq_dec rad 2) as [Hr2| Hr2]. {
        apply Q.nlt_ge in Hup; apply Hup; clear Hup.
        rewrite <- A_additive, Hnk.
