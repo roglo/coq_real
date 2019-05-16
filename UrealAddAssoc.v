cd80d1cfa98825485effced9db4840cc8609110f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-05-16 16:55:33 +0200

    -

--- UrealAddAssoc.v
+++ UrealAddAssoc.v
@@ -2825,12 +2825,6 @@ destruct H1 as [Huvn| H1]. {
       rewrite (proj1 Huv1), (proj2 Huv1), Hr2.
       replace (carry v (i + 1)) with 1; [ easy | symmetry ].
       unfold carry.
-Check fA_lt_1_ε_NQintg_A.
-rewrite (fA_lt_1_ε_NQintg_A 2 _ _ j).
-(* ça marche pas à cause du i+1 *)
-(* peut-être faudrait-il un lemme comme fA_lt_1_ε_NQintg_A mais
-   en plus général : s'il est vrai ! *)
-...
       clear Hjj Hj Hjk Hk Hauv Huv1 n Hn nj Hnj nk Hnk Huvl3.
       clear p Hjp Hp j k.
       revert i Hu Hv Huvn Hjq Hq Hs.
