021ed33ce01b2b436f9f1e90cd97c0b244b7e213
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-06-13 17:56:44 +0200

    -

--- cat2.v
+++ cat2.v
@@ -2,25 +2,27 @@ Require Import Utf8.
 
 Definition is_set (A : Type) := ∀ (a b : A) (p q : a = b), p = q.
 
-Record pre_cat :=
+Class pre_cat :=
   { obj : Type;
     morph : obj → obj → Type;
-    comp : ∀ a b c, morph b c → morph a b → morph a c;
-    id : ∀ a : obj, morph a a;
-    unit_l : ∀ (a b : obj) (f : morph a b), comp _ _ _ (id _) f = f;
-    unit_r : ∀ (a b : obj) (f : morph a b), comp _ _ _ f (id _) = f;
-    assoc : ∀ (a b c d : obj) (h : morph c d) (g : morph b c) (f : morph a b),
-        comp _ _ _ h (comp _ _ _ g f) = comp _ _ _ (comp _ _ _ h g) f;
-    homset : ∀ a b : obj, is_set (morph a b) }.
+    comp : ∀ {a b c}, morph a b → morph b c → morph a c;
+    id : ∀ {a}, morph a a;
+    unit_l : ∀ {a b} (f : morph a b), comp id f = f;
+    unit_r : ∀ {a b} (f : morph a b), comp f id = f;
+    assoc : ∀ {a b c d} (f : morph a b) (g : morph b c) (h : morph c d),
+      comp f (comp g h) = comp (comp f g) h;
+    homset : ∀ {a b}, is_set (morph a b) }.
 
+Arguments morph [_].
+Notation "g '◦' f" := (comp f g) (at level 40, left associativity).
 Coercion obj : pre_cat >-> Sortclass.
 
-Record is_isomorph {c : pre_cat} {a b : obj c} (f : morph c a b) :=
-  { g : morph c b a;
-    f_g : comp _ _ _ _ f g = id _ _;
-    g_f : comp _ _ _ _ g f = id _ _ }.
+Record is_isomorph {C : pre_cat} {a b : C} (f : morph a b) :=
+  { inv : morph b a;
+    comp_inv_l : comp inv f = id;
+    comp_inv_r : comp f inv = id }.
 
-Definition isomorphism {C : pre_cat} (a b : C) := { f : morph C a b & is_isomorph f }.
+Definition isomorphism {C : pre_cat} (a b : C) := { f : morph a b & is_isomorph f }.
 
 Definition isequiv {A B : Type} (f : A → B) :=
   {g : B → A & (∀ a, g (f a) = a) & (∀ b, f (g b) = b)}.
@@ -31,3 +33,9 @@ Notation "A ≃ B" := (equivalence A B) (at level 70).
 Record cat :=
   { pcat : pre_cat;
     univalent : ∀ a b : pcat, isomorphism a b ≃ (a = b) }.
+
+(* *)
+
+Definition catTyp A :=
+  {| obj := A;
+     morph a b := ... |}.
