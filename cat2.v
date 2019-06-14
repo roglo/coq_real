1e803b09cbcb99bbfd74625fcb5fa5b53b7fce14
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-06-14 17:47:12 +0200

    -

--- cat2.v
+++ cat2.v
@@ -27,30 +27,23 @@ Definition cTyp :=
      unit_r _ _ _ := eq_refl;
      assoc _ _ _ _ _ _ _ := eq_refl |}.
 
-(* tentative de faire Mat, mais c'est fatigant
-
-Require List.
-Import List.ListNotations.
-Open Scope list_scope.
-
-Definition vect n A := { v : list A | List.length v = n }.
-Record mat nrow ncol A := { mel : vect nrow (vect ncol A) }.
-
-Check (let l := [3; 4; 5] in (exist (λ l, List.length l = 3) l eq_refl) : vect _ _).
-
-Definition mat_mul A zero_A a b c (M : mat a b A) (N : mat b c A) (*: mat a c A*) : vect a A :=
-  exist (λ l, @List.length A l = a)
-    (List.repeat zero_A a) (List.repeat_length zero_A a).
-
-...
-
-Definition cMat A :=
-  {| obj := nat;
-     morph := mat A;
-     comp a b c M N := mat_mul A a b c M N |}.
-*)
+(* *)
 
 Definition is_initial {C : cat} (_0 : obj) :=
   ∀ c : obj, ∀ f g : morph _0 c, f = g.
 Definition is_final {C : cat} (_1 : obj) :=
   ∀ c : obj, ∀ f g : morph c _1, f = g.
+
+Record functor (C D : cat) :=
+  { f_obj : C → D;
+    f_arr {a b} : morph a b → morph (f_obj a) (f_obj b);
+    f_comp {a b c} (f : morph a b) (g : morph b c) :
+      f_arr (g ◦ f) = f_arr g ◦ f_arr f;
+    f_id {a} : @f_arr a _ id = id }.
+
+...
+
+(*
+Definition 5.17.LetJandCbe categories. AdiagramoftypeJinCis afunctor.D:J→C.
+*)
+Definition diagram (J C : cat) :=
