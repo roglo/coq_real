ddf48474d9b77f3852ea8e60d6c2034b57468f1f
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-06-27 18:14:47 +0200

    -

--- cat2.v
+++ cat2.v
@@ -262,47 +262,29 @@ Definition ap {A B x y} (f : A → B) (p : x = y) : f x = f y :=
 Definition homotopy {A B} (f g : A → B) := ∀ x : A, (f x = g x).
 Definition composite {A B C} (f : A → B) (g : B → C) x := g (f x).
 
-Definition qinv {A B} (f : A → B) :=
-  { g : B → A &
-   (homotopy (composite f g) (λ x, x) *
-    homotopy (composite g f) (λ x, x))%type }.
-
 Definition invert {A} {x y : A} (p : x = y) : y = x :=
   match p with
   | eq_refl _ => eq_refl x
   end.
 
-Definition isequiv_qinv {A B} (f : A → B) : isequiv f → qinv f.
-Proof.
-intros p.
-destruct p as (g, Hg, Hh).
-unfold qinv.
-econstructor; split; [ eassumption | ].
-intros x; apply Hh.
-Defined.
-
-Definition qinv_isequiv {A B} (f : A → B) : qinv f → isequiv f.
-Proof.
-intros p.
-destruct p as (g, (α, β)).
-econstructor; eassumption.
-Defined.
+Definition ap_composite {A B C x y}
+  : ∀ (f : A → B) (g : B → C) (p : x = y),
+    ap g (ap f p) = ap (composite f g) p
+  := λ f g p,
+     match p with eq_refl _ => eq_refl (ap g (ap f (eq_refl x))) end.
 
 Theorem hott_2_11_1 {A B} : ∀ (f : A → B), isequiv f → ∀ (a a' : A),
   (a = a') ≃ (f a = f a').
 Proof.
 intros f Hf a a'.
 exists (@ap A B a a' f).
-apply isequiv_qinv in Hf.
-destruct Hf as (f₁, (β, α)).
-apply qinv_isequiv.
-unfold qinv.
+destruct Hf as (f₁, β, α).
 set (g := λ r, compose (compose (invert (β a)) (ap f₁ r)) (β a')).
-unfold "◦", id in g; simpl in g.
-exists g; subst g.
-split; intros q.
+exists g; subst g; intros q.
+-destruct q; cbn.
+ now rewrite β.
 ...
- set (r := @compose _ _ _ a' (compose (@invert _ (f₁ (f a)) a (β a)) (ap f₁ q) (β a'))).
+-set (r := @compose _ _ _ a' (compose (@invert _ (f₁ (f a)) a (β a)) (ap f₁ q) (β a'))).
  apply (@compose _ _ ((α (f a))⁻¹ • α (f a) • ap f r)).
   eapply compose; [ apply lu | idtac ].
   apply dotr, invert, compose_invert_l.
@@ -348,13 +330,72 @@ split; intros q.
       rewrite <- H1.
       rewrite compose_assoc, compose_invert_l.
       reflexivity.
+*)
+Defined.
+...
+intros f Hf a a'.
+exists (@ap A B a a' f).
+destruct Hf as (f₁, β, α).
+unfold isequiv.
+set (g := λ r, compose (compose (invert (β a)) (ap f₁ r)) (β a')).
+exists g; subst g; intros q.
+(*
+ intros q.
+...
+ set (r := @compose _ _ _ a' (compose (@invert _ (f₁ (f a)) a (β a)) (ap f₁ q) (β a'))).
+ set (r := @compose _ _ _ a' (@invert _ (f₁ (f a)) a (β a) • ap f₁ q) (β a')).
+ apply (@compose _ _ ((α (f a))⁻¹ • α (f a) • ap f r)).
+  eapply compose; [ apply lu | idtac ].
+  apply dotr, invert, compose_invert_l.
+
+  eapply compose; [ eapply invert, compose_assoc | idtac ].
+  unfold id, composite; simpl.
+  pose proof (hott_2_4_3 ((f ◦ f₁) ◦ f) f (λ a, α (f a)) r) as H.
+  unfold "◦" in H; simpl in H.
+  eapply compose; [ eapply dotl, H | simpl ].
+  apply (@compose _ _ ((α (f a))⁻¹ • (ap f (ap f₁ (ap f r)) • α (f a')))).
+   apply dotl, dotr.
+   apply (@compose _ _ (ap (f ◦ f₁ ◦ f) r)); [ reflexivity | idtac ].
+   eapply invert, compose; [ idtac | eapply ap_composite ].
+   eapply compose; [ apply (ap_composite f₁ f (ap f r)) | reflexivity ].
+
+   eapply compose; [ apply compose_assoc | idtac ].
+   rewrite (ap_composite f f₁ r).
+   apply (@compose _ _ ((α (f a))⁻¹ • ap f (β a • r • (β a')⁻¹) • α (f a'))).
+    apply dotr, dotl, ap.
+    rewrite r; simpl.
+    rewrite <- ru, compose_invert_r.
+    reflexivity.
+
+    apply (@compose _ _ ((α (f a))⁻¹ • ap f (ap f₁ q) • α (f a'))).
+     apply dotr, dotl, ap; subst r.
+     do 2 rewrite compose_assoc.
+     rewrite compose_invert_r; simpl.
+     unfold id; simpl.
+     rewrite <- compose_assoc.
+     rewrite compose_invert_r; simpl.
+     rewrite <- ru; reflexivity.
+
+     assert (H1 : α (f a) • q = ap (f ◦ f₁) q • α (f a')).
+      rewrite <- (hott_2_4_3 (f ◦ f₁) id α q).
+      apply dotl, invert, hott_2_2_2_iv.
+
+      unfold id, composite; simpl.
+      pose proof (@ap_composite B A B (f a) (f a') f₁ f q) as H2.
+      rewrite H2.
+      rewrite <- compose_assoc.
+      unfold id, composite in H1; simpl in H1.
+      unfold composite; simpl.
+      rewrite <- H1.
+      rewrite compose_assoc, compose_invert_l.
+      reflexivity.
+*)
 
  rewrite (ap_composite f f₁ q).
  destruct q; simpl.
  unfold "◦", "∼", id in β; simpl in β.
  unfold "◦"; simpl; rewrite β; reflexivity.
 Defined.
-*)
 
 (*
 Lemma equiv_equiv_eq A B : ∀ (f : A → B) (g : B → A),
