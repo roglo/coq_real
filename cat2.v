3386dc8ef02a5071aac88f5bc55a7be8db8702c0
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-06-28 09:01:14 +0200

    -

--- cat2.v
+++ cat2.v
@@ -12,9 +12,9 @@ Class category :=
   { Obj : Type;
     Hom : Obj → Obj → Type;
     comp : ∀ {A B C}, Hom A B → Hom B C → Hom A C;
-    id : ∀ {A}, Hom A A;
-    unit_l : ∀ {A B} (f : Hom A B), comp id f = f;
-    unit_r : ∀ {A B} (f : Hom A B), comp f id = f;
+    hid : ∀ {A}, Hom A A;
+    unit_l : ∀ {A B} (f : Hom A B), comp hid f = f;
+    unit_r : ∀ {A B} (f : Hom A B), comp f hid = f;
     assoc : ∀ {A B C D} (f : Hom A B) (g : Hom B C) (h : Hom C D),
       comp f (comp g h) = comp (comp f g) h;
     Hom_set x y : is_set (Hom x y) }.
@@ -67,13 +67,13 @@ Class functor (C D : category) :=
     f_map_arr {a b} : Hom a b → Hom (f_map_obj a) (f_map_obj b);
     f_comp {a b c} (f : Hom a b) (g : Hom b c) :
       f_map_arr (g ◦ f) = f_map_arr g ◦ f_map_arr f;
-    f_id {a} : @f_map_arr a _ id = id }.
+    f_id {a} : @f_map_arr a _ hid = hid }.
 
 Arguments f_map_obj [_] [_] [_].
 Arguments f_map_arr [_] [_] _ [_] [_].
 
 Definition is_isomorphism {C : category} {A B : Obj} (f : Hom A B) :=
-  ∃ g : Hom B A, g ◦ f = id ∧ f ◦ g = id.
+  ∃ g : Hom B A, g ◦ f = hid ∧ f ◦ g = hid.
 
 (* *)
 
@@ -146,7 +146,7 @@ Definition cCone_comp {J C} {D : functor J C} (c c' c'' : cone D)
          (assoc (projT1 ch) (projT1 ch') (c_fam D c'' j))).
 
 Definition cCone_id {J C} {D : functor J C} (c : cone D) : cCone_Hom c c :=
-   existT (λ ϑ, ∀ j, c_fam D c j = c_fam D c j ◦ ϑ) id
+   existT (λ ϑ, ∀ j, c_fam D c j = c_fam D c j ◦ ϑ) hid
      (λ j, eq_sym (unit_l (c_fam D c j))).
 
 Theorem cCone_unit_l {J C} {D : functor J C} :
@@ -192,6 +192,215 @@ intros j.
 apply Hom_set.
 Defined.
 
+(**)
+
+Definition mid {A} (x : A) := x.
+
+Notation "'Σ' ( x : A ) , B" :=
+  ({ x : A & B }) (at level 0, x at level 0, B at level 100, only parsing).
+Notation "'Π' ( x : A ) , B" :=
+  (∀ x : A, B) (at level 0, x at level 0, B at level 100, only parsing).
+
+Definition homotopy {A B} (f g : A → B) := Π (x : A), (f x = g x).
+Notation "f '∼' g" := (homotopy f g) (at level 70).
+
+Definition composite {A B C} (f : A → B) (g : B → C) x := g (f x).
+Notation "g '◦◦' f" := (composite f g) (at level 40, left associativity).
+
+Definition isequiv {A B : Type} f :=
+  ((Σ (g : B → A), (f ◦◦ g ∼ mid)) * (Σ (h : B → A), (h ◦◦ f ∼ mid)))%type.
+
+Definition equivalence (A B : Type) := Σ (f : A → B), isequiv f.
+Notation "A ≃ B" := (equivalence A B) (at level 70).
+
+Definition ap {A B x y} (f : A → B) (p : x = y) : f x = f y :=
+  match p with
+  | eq_refl _ => eq_refl (f x)
+  end.
+
+Definition invert {A} {x y : A} (p : x = y) : y = x :=
+  match p with
+  | eq_refl _ => eq_refl x
+  end.
+Notation "p '⁻¹'" := (invert p)
+  (at level 8, left associativity, format "'[v' p ']' ⁻¹").
+
+Definition compose {A} {x y z : A} : x = y → y = z → x = z :=
+  λ p,
+  match p with
+  | eq_refl _ => mid
+  end.
+Notation "p • q" := (compose p q) (at level 40, left associativity).
+
+Definition qinv {A B} (f : A → B) :=
+  Σ (g : B → A), ((f ◦◦ g ∼ mid) * (g ◦◦ f ∼ mid))%type.
+
+Definition isequiv_qinv {A B} (f : A → B) : isequiv f → qinv f :=
+  λ p,
+  match p with
+  | (existT _ g Hg, existT _ h Hh) =>
+      existT _ g (Hg, λ x, ((ap h (Hg (f x)))⁻¹ • Hh (g (f x)))⁻¹ • Hh x)
+  end.
+
+Definition qinv_isequiv {A B} (f : A → B) : qinv f → isequiv f.
+Proof.
+intros p.
+destruct p as (g, (α, β)).
+split; exists g; assumption.
+Defined.
+
+Definition hott_2_1_4_i_1 {A} {x y : A} : ∀ (p : x = y),
+    p = p • eq_refl y
+ := (λ (p : x = y),
+     match p return (p = p • eq_refl _) with
+     | eq_refl _ => eq_refl (eq_refl x • eq_refl x)
+     end).
+
+Definition hott_2_1_4_i_2 {A} {x y : A} : ∀ (p : x = y),
+    p = eq_refl x • p
+ := (λ (p : x = y),
+   match p return (p = eq_refl _ • p) with
+   | eq_refl _ => eq_refl (eq_refl x • eq_refl x)
+   end).
+
+Definition lu {A} {b c : A} (r : b = c) : r = eq_refl b • r :=
+  hott_2_1_4_i_2 r.
+
+Definition ru {A} {a b : A} (p : a = b) : p = p • eq_refl b :=
+  hott_2_1_4_i_1 p.
+
+Definition dotl {A} {a b c : A} {r s : b = c}
+  (q : a = b) (β : r = s) : (q • r = q • s).
+Proof.
+destruct q.
+pose proof (@hott_2_1_4_i_2 A a c r) as H2.
+apply invert in H2.
+eapply compose; [ apply H2 | idtac ].
+pose proof (@hott_2_1_4_i_2 A a c s) as H4.
+eapply compose; [ apply β | apply H4 ].
+Defined.
+
+Definition dotr {A} {a b c : A} {p q : a = b}
+  (r : b = c) (α : p = q) : (p • r = q • r).
+Proof.
+destruct r.
+pose proof (@hott_2_1_4_i_1 A a b p) as H1.
+apply invert in H1.
+eapply compose; [ apply H1 | idtac ].
+pose proof (@hott_2_1_4_i_1 A a b q) as H3.
+eapply compose; [ apply α | apply H3 ].
+Defined.
+
+Lemma compose_invert_l {A} {x y : A} : ∀ (p : x = y), p⁻¹ • p = eq_refl y.
+Proof.
+intros p; destruct p; reflexivity.
+Qed.
+
+Definition compose_invert_r {A} {x y : A} : ∀ (p : x = y), p • p⁻¹ = eq_refl x
+  := λ p, match p with eq_refl _ => eq_refl (eq_refl x) end.
+
+Lemma compose_assoc {A} {x y z w : A} :
+  ∀ (p : x = y) (q : y = z) (r : z = w),
+  p • (q • r) = (p • q) • r.
+Proof.
+intros p q r; destruct p; reflexivity.
+Qed.
+
+Definition hott_2_4_3 {A B x y} (f g : A → B) (H : f ∼ g) (p : x = y)
+  : H x • ap g p = ap f p • H y
+  := match p with
+     | eq_refl _ =>
+         match
+           match H x as q return (q = q • eq_refl _) with
+           | eq_refl _ => eq_refl (eq_refl (f x) • eq_refl (f x))
+           end
+         with
+         | eq_refl _ => eq_refl (id (H x))
+         end
+     end.
+
+Definition ap_composite {A B C x y}
+  : ∀ (f : A → B) (g : B → C) (p : x = y),
+    ap g (ap f p) = ap (g ◦◦ f) p
+  := λ f g p,
+     match p with eq_refl _ => eq_refl (ap g (ap f (eq_refl x))) end.
+
+Definition hott_2_2_2_iv A (x y : A) : ∀ (p : x = y), ap mid p = p
+  := λ p, match p with eq_refl _ => eq_refl (eq_refl x) end.
+
+Theorem hott_2_11_1 {A B} : ∀ (f : A → B), isequiv f → ∀ (a a' : A),
+  (a = a') ≃ (f a = f a').
+Proof.
+intros f Hf a a'.
+exists (@ap A B a a' f).
+apply isequiv_qinv in Hf.
+destruct Hf as (f₁, (α, β)).
+apply qinv_isequiv.
+unfold qinv.
+set (g := λ r, (β a)⁻¹ • ap f₁ r • β a').
+unfold "◦◦", mid in g; simpl in g.
+exists g; subst g.
+unfold "◦◦", "∼", id; simpl.
+split; intros q.
+-set (r := @compose _ _ _ a' (@invert _ (f₁ (f a)) a (β a) • ap f₁ q) (β a')).
+ apply (@compose _ _ ((α (f a))⁻¹ • α (f a) • ap f r)).
+ +eapply compose; [ apply lu | idtac ].
+  apply dotr, invert, compose_invert_l.
+ +eapply compose; [ eapply invert, compose_assoc | idtac ].
+  unfold mid, composite; simpl.
+  pose proof (hott_2_4_3 ((f ◦◦ f₁) ◦◦ f) f (λ a, α (f a)) r) as H.
+  unfold "◦" in H; simpl in H.
+  eapply compose; [ eapply dotl, H | simpl ].
+  apply (@compose _ _ ((α (f a))⁻¹ • (ap f (ap f₁ (ap f r)) • α (f a')))).
+  *apply dotl, dotr.
+   apply (@compose _ _ (ap (f ◦◦ f₁ ◦◦ f) r)); [ reflexivity | idtac ].
+   eapply invert, compose; [ idtac | eapply ap_composite ].
+   eapply compose; [ apply (ap_composite f₁ f (ap f r)) | reflexivity ].
+  *eapply compose; [ apply compose_assoc | idtac ].
+   rewrite (ap_composite f f₁ r).
+   apply (@compose _ _ ((α (f a))⁻¹ • ap f (β a • r • (β a')⁻¹) • α (f a'))).
+  --apply dotr, dotl, ap.
+    rewrite r; simpl.
+    rewrite <- ru, compose_invert_r.
+    reflexivity.
+  --apply (@compose _ _ ((α (f a))⁻¹ • ap f (ap f₁ q) • α (f a'))).
+   **apply dotr, dotl, ap; subst r.
+     do 2 rewrite compose_assoc.
+     rewrite compose_invert_r; simpl.
+     unfold mid; simpl.
+     rewrite <- compose_assoc.
+     rewrite compose_invert_r; simpl.
+     rewrite <- ru; reflexivity.
+   **assert (H1 : α (f a) • q = ap (f ◦◦ f₁) q • α (f a')). {
+       rewrite <- (hott_2_4_3 (f ◦◦ f₁) mid α q).
+       apply dotl, invert, hott_2_2_2_iv.
+     }
+     unfold mid, composite; simpl.
+     pose proof (@ap_composite B A B (f a) (f a') f₁ f q) as H2.
+     rewrite H2.
+     rewrite <- compose_assoc.
+     unfold mid, composite in H1; simpl in H1.
+     unfold composite; simpl.
+     rewrite <- H1.
+     rewrite compose_assoc, compose_invert_l.
+     reflexivity.
+-rewrite (ap_composite f f₁ q).
+ destruct q; simpl.
+ unfold "◦◦", "∼", id in β; simpl in β.
+ unfold "◦◦"; simpl; rewrite β; reflexivity.
+Defined.
+
+Definition quasi_inv {A B} : A ≃ B → B ≃ A :=
+  λ eqf,
+  match eqf with
+  | existT _ f (existT _ g Hg, existT _ h Hh) =>
+      existT isequiv g
+        (existT _ f (λ x, (Hh (g (f x)))⁻¹ • ap h (Hg (f x)) • Hh x),
+         existT _ f Hg)
+ end.
+
+(**)
+
 Definition isProp (A : Type) := ∀ (x y : A), x = y.
 
 Definition isContr A := {a : A & ∀ x : A, a = x }.
@@ -201,9 +410,6 @@ Fixpoint isnType A n :=
   | 0 => isProp A
   | S p' => ∀ x y : A, isnType (x = y) p'
   end.
-
-Definition compose {A} {x y z : A} : x = y → y = z → x = z :=
-  λ p, match p with eq_refl _ => λ x, x end.
 
 Theorem compose_cancel_l {A} {x y z : A} (p : x = y) (q r : y = z) :
   compose p q = compose p r → q = r.
@@ -234,18 +440,6 @@ Definition transport {A} P {x y : A} (p : x = y) : P x → P y :=
   | eq_refl _ => λ x, x
   end.
 
-Definition isequiv {A B : Type} (f : A → B) :=
-  {g : B → A & (∀ a, g (f a) = a) & (∀ b, f (g b) = b)}.
-
-Definition equivalence (A B : Type) := { f : A → B & isequiv f}.
-Notation "A ≃ B" := (equivalence A B) (at level 70).
-
-Theorem equiv_sym : ∀ A B, A ≃ B → B ≃ A.
-Proof.
-intros * (f, (g, Hgf, Hfg)).
-now exists g, f.
-Qed.
-
 Theorem equiv_eq : ∀ A B (f : A → B) (g : B → A) x y,
   (∀ b, f (g b) = b) → g x = g y → x = y.
 Proof.
@@ -254,185 +448,18 @@ apply @compose with (y := f (g y)); [ | apply Hfg ].
 destruct H; symmetry; apply Hfg.
 Defined.
 
-Definition ap {A B x y} (f : A → B) (p : x = y) : f x = f y :=
-  match p with
-  | eq_refl _ => eq_refl (f x)
-  end.
-
-Definition homotopy {A B} (f g : A → B) := ∀ x : A, (f x = g x).
-Definition composite {A B C} (f : A → B) (g : B → C) x := g (f x).
-
-Definition invert {A} {x y : A} (p : x = y) : y = x :=
-  match p with
-  | eq_refl _ => eq_refl x
-  end.
-
-Definition ap_composite {A B C x y}
-  : ∀ (f : A → B) (g : B → C) (p : x = y),
-    ap g (ap f p) = ap (composite f g) p
-  := λ f g p,
-     match p with eq_refl _ => eq_refl (ap g (ap f (eq_refl x))) end.
-
-Theorem hott_2_11_1 {A B} : ∀ (f : A → B), isequiv f → ∀ (a a' : A),
-  (a = a') ≃ (f a = f a').
-Proof.
-intros f Hf a a'.
-exists (@ap A B a a' f).
-destruct Hf as (f₁, β, α).
-set (g := λ r, compose (compose (invert (β a)) (ap f₁ r)) (β a')).
-exists g; subst g; intros q.
--destruct q; cbn.
- now rewrite β.
-...
--set (r := @compose _ _ _ a' (compose (@invert _ (f₁ (f a)) a (β a)) (ap f₁ q) (β a'))).
- apply (@compose _ _ ((α (f a))⁻¹ • α (f a) • ap f r)).
-  eapply compose; [ apply lu | idtac ].
-  apply dotr, invert, compose_invert_l.
-
-  eapply compose; [ eapply invert, compose_assoc | idtac ].
-  unfold id, composite; simpl.
-  pose proof (hott_2_4_3 ((f ◦ f₁) ◦ f) f (λ a, α (f a)) r) as H.
-  unfold "◦" in H; simpl in H.
-  eapply compose; [ eapply dotl, H | simpl ].
-  apply (@compose _ _ ((α (f a))⁻¹ • (ap f (ap f₁ (ap f r)) • α (f a')))).
-   apply dotl, dotr.
-   apply (@compose _ _ (ap (f ◦ f₁ ◦ f) r)); [ reflexivity | idtac ].
-   eapply invert, compose; [ idtac | eapply ap_composite ].
-   eapply compose; [ apply (ap_composite f₁ f (ap f r)) | reflexivity ].
-
-   eapply compose; [ apply compose_assoc | idtac ].
-   rewrite (ap_composite f f₁ r).
-   apply (@compose _ _ ((α (f a))⁻¹ • ap f (β a • r • (β a')⁻¹) • α (f a'))).
-    apply dotr, dotl, ap.
-    rewrite r; simpl.
-    rewrite <- ru, compose_invert_r.
-    reflexivity.
-
-    apply (@compose _ _ ((α (f a))⁻¹ • ap f (ap f₁ q) • α (f a'))).
-     apply dotr, dotl, ap; subst r.
-     do 2 rewrite compose_assoc.
-     rewrite compose_invert_r; simpl.
-     unfold id; simpl.
-     rewrite <- compose_assoc.
-     rewrite compose_invert_r; simpl.
-     rewrite <- ru; reflexivity.
-
-     assert (H1 : α (f a) • q = ap (f ◦ f₁) q • α (f a')).
-      rewrite <- (hott_2_4_3 (f ◦ f₁) id α q).
-      apply dotl, invert, hott_2_2_2_iv.
-
-      unfold id, composite; simpl.
-      pose proof (@ap_composite B A B (f a) (f a') f₁ f q) as H2.
-      rewrite H2.
-      rewrite <- compose_assoc.
-      unfold id, composite in H1; simpl in H1.
-      unfold composite; simpl.
-      rewrite <- H1.
-      rewrite compose_assoc, compose_invert_l.
-      reflexivity.
-*)
-Defined.
-...
-intros f Hf a a'.
-exists (@ap A B a a' f).
-destruct Hf as (f₁, β, α).
-unfold isequiv.
-set (g := λ r, compose (compose (invert (β a)) (ap f₁ r)) (β a')).
-exists g; subst g; intros q.
-(*
- intros q.
-...
- set (r := @compose _ _ _ a' (compose (@invert _ (f₁ (f a)) a (β a)) (ap f₁ q) (β a'))).
- set (r := @compose _ _ _ a' (@invert _ (f₁ (f a)) a (β a) • ap f₁ q) (β a')).
- apply (@compose _ _ ((α (f a))⁻¹ • α (f a) • ap f r)).
-  eapply compose; [ apply lu | idtac ].
-  apply dotr, invert, compose_invert_l.
-
-  eapply compose; [ eapply invert, compose_assoc | idtac ].
-  unfold id, composite; simpl.
-  pose proof (hott_2_4_3 ((f ◦ f₁) ◦ f) f (λ a, α (f a)) r) as H.
-  unfold "◦" in H; simpl in H.
-  eapply compose; [ eapply dotl, H | simpl ].
-  apply (@compose _ _ ((α (f a))⁻¹ • (ap f (ap f₁ (ap f r)) • α (f a')))).
-   apply dotl, dotr.
-   apply (@compose _ _ (ap (f ◦ f₁ ◦ f) r)); [ reflexivity | idtac ].
-   eapply invert, compose; [ idtac | eapply ap_composite ].
-   eapply compose; [ apply (ap_composite f₁ f (ap f r)) | reflexivity ].
-
-   eapply compose; [ apply compose_assoc | idtac ].
-   rewrite (ap_composite f f₁ r).
-   apply (@compose _ _ ((α (f a))⁻¹ • ap f (β a • r • (β a')⁻¹) • α (f a'))).
-    apply dotr, dotl, ap.
-    rewrite r; simpl.
-    rewrite <- ru, compose_invert_r.
-    reflexivity.
-
-    apply (@compose _ _ ((α (f a))⁻¹ • ap f (ap f₁ q) • α (f a'))).
-     apply dotr, dotl, ap; subst r.
-     do 2 rewrite compose_assoc.
-     rewrite compose_invert_r; simpl.
-     unfold id; simpl.
-     rewrite <- compose_assoc.
-     rewrite compose_invert_r; simpl.
-     rewrite <- ru; reflexivity.
-
-     assert (H1 : α (f a) • q = ap (f ◦ f₁) q • α (f a')).
-      rewrite <- (hott_2_4_3 (f ◦ f₁) id α q).
-      apply dotl, invert, hott_2_2_2_iv.
-
-      unfold id, composite; simpl.
-      pose proof (@ap_composite B A B (f a) (f a') f₁ f q) as H2.
-      rewrite H2.
-      rewrite <- compose_assoc.
-      unfold id, composite in H1; simpl in H1.
-      unfold composite; simpl.
-      rewrite <- H1.
-      rewrite compose_assoc, compose_invert_l.
-      reflexivity.
-*)
-
- rewrite (ap_composite f f₁ q).
- destruct q; simpl.
- unfold "◦", "∼", id in β; simpl in β.
- unfold "◦"; simpl; rewrite β; reflexivity.
-Defined.
-
-(*
-Lemma equiv_equiv_eq A B : ∀ (f : A → B) (g : B → A),
-  (∀ a, g (f a) = a) → (∀ b, f (g b) = b) →
-  ∀ x y, (f x = f y) ≃ (x = y).
-Proof.
-intros * Hgf Hfg *.
-exists (equiv_eq B A g f x y Hgf).
-unfold isequiv.
-exists (@f_equal _ _ f x y). {
-  intros p.
-  assert (H : x = y). {
-    transitivity (g (f x)); [ symmetry; apply Hgf | ].
-    transitivity (g (f y)); [ | apply Hgf ].
-    now f_equal.
-  }
-  remember (equiv_eq B A g f x y Hgf p) as q eqn:Hq.
-  destruct q; cbn.
-  unfold equiv_eq in Hq.
-  destruct (Hgf x); cbn in Hq.
-  rewrite compose_eq_refl in Hq.
-...
-}
-intros p.
-subst y; cbn.
-unfold equiv_eq.
-now destruct (Hgf x).
-...
-*)
-
 Lemma isnType_if_equiv : ∀ A B n, A ≃ B → isnType A n → isnType B n.
 Proof.
 intros * HAB HA.
 revert A B HAB HA.
 induction n; intros. {
   destruct HAB as (f, Hf).
+  destruct Hf as ((g, Hfg), (h, Hhf)).
+(*
   destruct Hf as (g, Hgf, Hfg).
+*)
+  move h before g.
+  unfold mid, "◦◦", "∼" in Hfg, Hhf.
   cbn in HA |-*.
   unfold isProp in HA |-*.
   intros x y.
@@ -442,14 +469,28 @@ induction n; intros. {
   apply HA.
 }
 destruct HAB as (f, Hf).
+destruct Hf as ((g, Hfg), (h, Hhf)).
+(*
 destruct Hf as (g, Hgf, Hfg).
+*)
 cbn in HA |-*.
+move h before g.
+unfold mid, "◦◦", "∼" in Hfg, Hhf.
 intros x y.
 apply (IHn (g x = g y) (x = y)); [ | apply HA ].
-apply equiv_sym.
+apply quasi_inv.
 apply hott_2_11_1.
-now exists f.
-...
+split.
+-exists f.
+ unfold "◦◦", "∼", mid.
+ intros z.
+ rewrite <- Hhf.
+ clear - Hfg Hhf.
+ f_equal. (* bizarre que ça marche *)
+-exists f.
+ unfold "◦◦", "∼", mid.
+ apply Hfg.
+Qed.
 
 Theorem isnType_isnType_sigT (A : Type) : ∀ n P,
   (∀ x, isProp (P x)) → isnType A n → isnType (@sigT A P) n.
@@ -489,10 +530,7 @@ specialize (H4 H); clear H.
 cbn in Hn.
 specialize (H4 (Hn a b)).
 subst Q.
-...
-
-Check {p : a = b & transport P p Ha = Hb}.
-Check (existT P a Ha = existT P b Hb).
+eapply isnType_if_equiv; [ | apply H4 ].
 ...
 
 Theorem is_set_is_set_sigT (A : Type) : ∀ P, is_set A → is_set (@sigT A P).
