92fa5f2327ae6ccc4cdd734bf78141c0058a3d8b
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-09-11 16:20:41 +0200

    -

--- prime.v
+++ prime.v
@@ -467,6 +467,14 @@ assert
   replace (2 * S n) with (2 + n * 2) by flia.
   now rewrite f_add_opp_diag_l, f_add_0_l.
 }
+(* ζ_2n(s) - 1/(2^s.ζ_n(s)) = 1 + 1/3^s + 1/5^s + 1/7^s + ... + 1/(2n-1)^s
+   ζ_n(s) - 1/(2^s.ζ_n(s)) + ζ_{n+1,2n}(s) = 1 + 1/3^s + 1/5^s + 1/7^s + ... + 1/(2n-1)^s
+   (1 - 1/2^s)ζ_n(s) + ζ_{n+1,2n}(s) = 1 + 1/3^s + 1/5^s + 1/7^s + ... + 1/(2n-1)^s *)
+(* the term ζ_{n+1,2n)(s) = 1/(n+1)^s + 1/(n+2)^s + ... 1/(2n)^s is
+   embarrassing *)
+(* but, in general, how can I compare two expressions? they are supposed
+   to go to infinity, but I cannot do that, and up to some n, they are not
+   comparable *)
 ...
 
 Theorem zeta_Euler_product_eq : ∀ s, expr_eq (zeta s) (zeta' s).
