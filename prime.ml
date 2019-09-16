510b6b0cc57072d5d920d626ca1ba034df01cc9d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-09-16 09:03:07 +0200

    -

--- prime.ml
+++ prime.ml
@@ -14,7 +14,7 @@ type ln_polyn =
 value f_zero = 0.;
 value f_one = 1.;
 
-value zeta = { ls _ = f_one }.
+value ζ = { ls _ = f_one }.
 
 value log_prod_tail_rec u v n =
   log_prod_loop 0. where rec log_prod_loop accu i =
@@ -22,6 +22,7 @@ value log_prod_tail_rec u v n =
     | 0 → accu
     | _ →
         let i' = i - 1 in
+	let q = (n + 1) / i in
         let accu = accu +.
 	  match (n + 1) mod i with
 	  | 0 → u i' *. v ((n + 1) / i - 1)
@@ -51,13 +52,13 @@ value ls_of_pol p =
 value ls_pol_mul_l p s =
   ls_mul (ls_of_pol p) s.
 
-value zeta_but_mul_of d =
+value ζ_but_mul_of d =
   { ls n =
       match (n + 1) mod d with
       | 0 → f_zero
       | _ → f_one
       end }.
 
-value f1 = zeta_but_mul_of 2;
-value f2 = ls_pol_mul_l { lp = [f_one; -. f_one] } zeta;
+value f1 = ζ_but_mul_of 2;
+value f2 = ls_pol_mul_l { lp = [f_one; -. f_one] } ζ;
 
