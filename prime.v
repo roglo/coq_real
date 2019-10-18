a4a66257a6af1757c777bc9aac3a21864c491329
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-10-18 08:40:47 +0200

    -

--- prime.v
+++ prime.v
@@ -123,51 +123,16 @@ Theorem prime_divisor : ∀ n, 2 ≤ n →
   ∃ d, is_prime d = true ∧ Nat.divide d n.
 Proof.
 intros * Hn.
+induction n as (n, IHn) using (well_founded_ind lt_wf).
 remember (is_prime n) as b eqn:Hb; symmetry in Hb.
 destruct b; [ now exists n | ].
 specialize (not_prime_exists_div n Hn Hb) as (a & Han & Hd).
-move a before n; move Han before Hn.
-remember (is_prime a) as b1 eqn:Hb1; symmetry in Hb1.
-destruct b1; [ now exists a | move Hb1 before Hb ].
-specialize (not_prime_exists_div a (proj1 Han) Hb1) as (a2 & Ha2a & Hd2).
-move a2 before a; move Ha2a before Han.
-apply (Nat.divide_trans a2 _ n) in Hd2; [ | easy ].
-remember (is_prime a2) as b2 eqn:Hb2; symmetry in Hb2.
-destruct b2; [ now exists a2 | move Hb2 before Hb1 ].
-specialize (not_prime_exists_div a2 (proj1 Ha2a) Hb2) as (a3 & Ha3a & Hd3).
-move a3 before a2; move Ha3a before Ha2a.
-apply (Nat.divide_trans a3 _ n) in Hd3; [ | easy ].
-remember (is_prime a3) as b3 eqn:Hb3; symmetry in Hb3.
-destruct b3; [ now exists a3 | move Hb3 before Hb2 ].
-specialize (not_prime_exists_div a3 (proj1 Ha3a) Hb3) as (a4 & Ha4a & Hd4).
-move a4 before a3; move Ha4a before Ha3a.
-apply (Nat.divide_trans a4 _ n) in Hd4; [ | easy ].
-remember (is_prime a4) as b4 eqn:Hb4; symmetry in Hb4.
-destruct b4; [ now exists a4 | move Hb4 before Hb3 ].
-...
-induction n as (n, IHn) using (well_founded_ind lt_wf).
-apply (well_founded_ind lt_wf).
-Print Wf.
-Check well_founded_ind.
-Require Wf.
-Search Wf.well_founded.
-specialize (well_founded_ind lt_wf (λ n, n < 2 ∨ is_prime n = true)) as H1.
-cbn in H1.
-assert (∀ x, (∀ y, y < x → y < 2 ∨ is_prime y = true) → x < 2 ∨ is_prime x = true). {
-  clear.
-  intros n Hn.
-  specialize (Decidable.not_and (2 ≤ n) (is_prime n = false)) as H1.
-  specialize (H1 (Nat.le_decidable _ _)).
-  enough (¬ 2 ≤ n ∨ is_prime n ≠ false). {
-    destruct H as [H| H]; [ now left; apply Nat.nle_gt | ].
-    now right; apply Bool.not_false_is_true.
-  }
-  apply H1; clear H1.
-  intros (H1, H2).
-  apply not_prime_exists_div in H2; [ | easy ].
-  destruct H2 as (a & b & Ha & Hb & Hnab).
-  specialize (Hn a Ha) as H2.
-...
+specialize (IHn a (proj2 Han) (proj1 Han)) as H1.
+destruct H1 as (d & Hpd & Hda).
+exists d.
+split; [ easy | ].
+now transitivity a.
+Qed.
 
 Theorem Nat_fact_succ : ∀ n, fact (S n) = S n * fact n.
 Proof. easy. Qed.
